0181719a6d4a3603f268c8495a270bdf25ae8c19
georgeweigt <yo73137@gmail.com>
2020-01-27 17:25:29 -0700

    update

--- tools/gen.c
+++ tools/gen.c
@@ -1,138 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 
+int filter(const struct dirent *p);
 void emit_file(char *);
 void emit_line(char *);
 void check_line(char *, int, char *);
 
 int prev = '\n';
+char filename[100];
 
 #define PATH "../src/"
-
-char *filename[] = {
-	PATH "defs1.h",
-	PATH "prototypes.h",
-	PATH "globals.c",
-	PATH "main.c",
-	PATH "run.c",
-	PATH "scan.c",
-	PATH "eval.c",
-	PATH "display.c",
-	PATH "abs.c",
-	PATH "add.c",
-	PATH "adj.c",
-	PATH "arccos.c",
-	PATH "arccosh.c",
-	PATH "arcsin.c",
-	PATH "arcsinh.c",
-	PATH "arctan.c",
-	PATH "arctanh.c",
-	PATH "arg.c",
-	PATH "atomize.c",
-	PATH "bake.c",
-	PATH "besselj.c",
-	PATH "bessely.c",
-	PATH "binomial.c",
-	PATH "ceiling.c",
-	PATH "choose.c",
-	PATH "circexp.c",
-	PATH "clock.c",
-	PATH "coeff.c",
-	PATH "cofactor.c",
-	PATH "conj.c",
-	PATH "contract.c",
-	PATH "cos.c",
-	PATH "cosh.c",
-	PATH "defint.c",
-	PATH "degree.c",
-	PATH "denominator.c",
-	PATH "derivative.c",
-	PATH "det.c",
-	PATH "distill.c",
-	PATH "divisors.c",
-	PATH "eigen.c",
-	PATH "erf.c",
-	PATH "erfc.c",
-	PATH "exp.c",
-	PATH "expand.c",
-	PATH "expcos.c",
-	PATH "expsin.c",
-	PATH "factor.c",
-	PATH "factorial.c",
-	PATH "factorpoly.c",
-	PATH "factors.c",
-	PATH "filter.c",
-	PATH "float.c",
-	PATH "floor.c",
-	PATH "for.c",
-	PATH "gcd.c",
-	PATH "guess.c",
-	PATH "hermite.c",
-	PATH "hilbert.c",
-	PATH "imag.c",
-	PATH "index.c",
-	PATH "inner.c",
-	PATH "integral.c",
-	PATH "inv.c",
-	PATH "is.c",
-	PATH "isprime.c",
-	PATH "itab.c",
-	PATH "laguerre.c",
-	PATH "lcm.c",
-	PATH "leading.c",
-	PATH "legendre.c",
-	PATH "log.c",
-	PATH "mag.c",
-	PATH "mod.c",
-	PATH "multiply.c",
-	PATH "nroots.c",
-	PATH "numerator.c",
-	PATH "outer.c",
-	PATH "partition.c",
-	PATH "polar.c",
-	PATH "pollard.c",
-	PATH "power.c",
-	PATH "prime.c",
-	PATH "primetab.c",
-	PATH "print.c",
-	PATH "product.c",
-	PATH "quotient.c",
-	PATH "rationalize.c",
-	PATH "real.c",
-	PATH "rect.c",
-	PATH "roots.c",
-	PATH "setq.c",
-	PATH "sgn.c",
-	PATH "simfac.c",
-	PATH "simplify.c",
-	PATH "sin.c",
-	PATH "sinh.c",
-	PATH "sum.c",
-	PATH "tan.c",
-	PATH "tanh.c",
-	PATH "taylor.c",
-	PATH "tensor.c",
-	PATH "test.c",
-	PATH "transform.c",
-	PATH "transpose.c",
-	PATH "userfunc.c",
-	PATH "zero.c",
-	PATH "core.c",
-	PATH "symbol.c",
-	PATH "stack.c",
-	PATH "bignum.c",
-	NULL
-};
 
 int
 main()
 {
-	int i = 0;
+	int i, n;
+	struct dirent **p;
 	system("cat preamble.h");
-	while (filename[i])
-		emit_file(filename[i++]);
+	emit_file(PATH "defs1.h");
+	emit_file(PATH "prototypes.h");
+	emit_file(PATH "globals.c");
+	n = scandir("../src", &p, filter, alphasort);
+	for (i = 0; i < n; i++) {
+		if (strcmp(p[i]->d_name, "globals.c") == 0)
+			continue;
+		strcpy(filename, "../src/");
+		strcat(filename, p[i]->d_name);
+		emit_file(filename);
+	}
 	return 0;
+}
+
+int
+filter(const struct dirent *p)
+{
+	int len = strlen(p->d_name);
+
+	if (len > 2 && strcmp(p->d_name + len - 2, ".c") == 0)
+		return 1;
+	else
+		return 0;
 }
 
 void
