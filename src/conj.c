de87bbb185f658d1b2f8570fdedc6f7f49b2c20d
georgeweigt <yo73137@gmail.com>
2020-04-06 07:13:25 -0700

    Update conj.c

--- src/conj.c
+++ src/conj.c
@@ -48,7 +48,7 @@ conjugate_subst_nib(void)
 
 	if (car(p1) == symbol(POWER) && isminusone(cadr(p1))) {
 		push_symbol(POWER);
-		push(cadr(p1));
+		push(minusone);
 		push(caddr(p1));
 		negate();
 		list(3);
