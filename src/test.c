00b224af54d601aca4f737050c0d8e3c09c987bc
georgeweigt <yo73137@gmail.com>
2020-07-29 11:08:52 -0700

    bug fix

--- src/test.c
+++ src/test.c
@@ -170,7 +170,7 @@ cross_factor(struct atom *p)
 	if (isrational(p)) {
 		if (MEQUAL(p->u.q.b, 1))
 			return 0;
-		push_rational_number(MPLUS, mcopy(p1->u.q.b), mint(1));
+		push_rational_number(MPLUS, mcopy(p->u.q.b), mint(1));
 		return 1;
 	}
 
