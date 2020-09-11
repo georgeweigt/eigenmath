c37e19ff1c75865ae36bd1668b556e1652b4bf79
georgeweigt <yo73137@gmail.com>
2020-09-11 08:44:14 -0700

    Update eval_unit.js

--- js/eval_unit.js
+++ js/eval_unit.js
@@ -8,12 +8,12 @@ eval_unit(p1)
 	n = pop_integer();
 
 	if (n < 2)
-		stop("index range error");
+		stop("index err");
 
 	p1 = alloc_tensor();
 
 	p1.dim[0] = n;
-	p2.dim[1] = n;
+	p1.dim[1] = n;
 
 	for (i = 0; i < n; i++)
 		for (j = 0; j < n; j++)
