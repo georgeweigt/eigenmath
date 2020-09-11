eb2e62ca5fd16f4b482a82e76651bece00e9e2ab
georgeweigt <yo73137@gmail.com>
2020-09-11 08:40:50 -0700

    Update eval_zero.js

--- js/eval_zero.js
+++ js/eval_zero.js
@@ -3,25 +3,24 @@ eval_zero(p1)
 {
 	var i, m, n, p2;
 
-	if (length(p1) < 2)
-		stop("argument expected");
-
 	p1 = cdr(p1);
 	p2 = alloc_tensor();
+
+	m = 1;
 
 	while (iscons(p1)) {
 		push(car(p1));
 		evalf();
 		n = pop_integer();
 		if (n < 2)
-			stop("dimension range error");
+			stop("index err");
 		p2.dim.push(n);
 		m *= n;
 		p1 = cdr(p1);
 	}
 
 	for (i = 0; i < m; i++)
-		p1.elem[i] = zero;
+		p2.elem[i] = zero;
 
-	push(p1);
+	push(p2);
 }
