fafd16795a5b0e6be5198ce99e86b84ecf035def
georgeweigt <yo73137@gmail.com>
2020-09-12 08:54:25 -0700

    update

--- js/isdenominator.js
+++
@@ -1,5 +0,0 @@
-function
-isdenominator(p)
-{
-	return car(p) == symbol(POWER) && cadr(p) != symbol(EXP1) && isnegativeterm(caddr(p));
-}
--- js/iseveninteger.js
+++
@@ -1,5 +0,0 @@
-function
-iseveninteger(p)
-{
-	return isinteger(p) && Math.mod(p.a, 2) == 0;
-}
