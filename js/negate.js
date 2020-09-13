13ba4738d64c4b9480a82b495ab9b02dc3c7c298
georgeweigt <yo73137@gmail.com>
2020-09-13 06:57:49 -0700

    Update negate.js

--- js/negate.js
+++ js/negate.js
@@ -1,15 +1,19 @@
 function
 negate()
 {
-	if (isnum(stack[stack.length - 1])) {
-		var p1 = pop();
-		if (isrational(p1))
-			push_rational(-p1.a, p1.b);
-		else
-			push_double(-p1.d);
+	var p1 = pop();
+
+	if (isrational(p1)) {
+		push_rational(-p1.a, p1.b);
+		return;
+	}
+
+	if (isdouble(p1)) {
+		push_double(-p1.d);
 		return;
 	}
 
 	push_integer(-1);
+	push(p1);
 	multiply();
 }
