0ef488babbe6f2fca9d8763d2959b4b472a74511
georgeweigt <yo73137@gmail.com>
2020-10-18 08:58:19 -0700

    update

--- js/emit_arglist.js
+++ js/emit_arglist.js
@@ -1,8 +1,10 @@
 function
 emit_arglist(u, p, small_font)
 {
-	var v = {type:PAREN, a:[]};
+	var v = {type:PAREN, a:[], small_font:small_font};
+
 	p = cdr(p);
+
 	if (iscons(p)) {
 		emit_expr(u, car(p), small_font);
 		p = cdr(p);
@@ -13,10 +15,13 @@ emit_arglist(u, p, small_font)
 			p = cdr(p);
 		}
 	}
+
 	emit_update(v);
+
 	if (small_font)
 		v.width += 2 * SMALL_FONT_WIDTH;
 	else
 		v.width += 2 * FONT_WIDTH;
+
 	u.a.push(v);
 }
--- js/emit_index_list.js
+++ js/emit_index_list.js
@@ -1,22 +1,27 @@
 function
 emit_index_list(u, p, small_font)
 {
-	var v = {type:BRACK, a:[]};
+	var v = {type:BRACK, a:[], small_font:small_font};
+
 	p = cdr(p);
+
 	if (iscons(p)) {
 		emit_expr(u, car(p), small_font);
 		p = cdr(p);
 		while (iscons(p)) {
 			emit_roman_text(u, ",", small_font);
-			emit_thin_space(u);
+			emit_thin_space(u, small_font);
 			emit_expr(u, car(p), small_font);
 			p = cdr(p);
 		}
 	}
+
 	emit_update(v);
+
 	if (small_font)
 		v.width += 2 * SMALL_FONT_WIDTH;
 	else
 		v.width += 2 * FONT_WIDTH;
+
 	u.a.push(v);
 }
