d121fe15d5eed2638dc532f6aac6a12d0c1a0ed9
georgeweigt <yo73137@gmail.com>
2020-10-22 09:40:49 -0700

    Update emit_update_text.js

--- js/emit_update_text.js
+++ js/emit_update_text.js
@@ -3,14 +3,17 @@ emit_update_text(u)
 {
 	var n, r, size;
 
-	n = u.s.charCodeAt(0);
-
-	r = emit_wtab[n];
-
 	if (u.small_font)
 		size = SMALL_FONT_SIZE;
 	else
 		size = FONT_SIZE;
+
+	n = u.s.charCodeAt(0);
+
+	if (n < 128)
+		r = emit_wtab[n];
+	else
+		r = 1.0;
 
 	u.height = HEIGHT_RATIO * size;
 	u.depth = DEPTH_RATIO * size;
