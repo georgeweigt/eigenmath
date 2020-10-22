22a426b85389465e5b561ab2432952a8c180021d
georgeweigt <yo73137@gmail.com>
2020-10-22 15:18:27 -0700

    Update emit_svg_parens.js

--- js/emit_svg_parens.js
+++ js/emit_svg_parens.js
@@ -1,15 +1,20 @@
 function
 emit_svg_parens(p, x, y)
 {
-	var l, r, w;
+	var d, h, l, r, w;
 
-	if (p.small_font)
+	if (p.small_font) {
+		h = HEIGHT_RATIO * SMALL_FONT_SIZE;
+		d = DEPTH_RATIO * SMALL_FONT_SIZE;
 		w = SMALL_DELIM_WIDTH;
-	else
+	} else {
+		h = HEIGHT_RATIO * FONT_SIZE;
+		d = DEPTH_RATIO * FONT_SIZE;
 		w = DELIM_WIDTH;
+	}
 
-	l = {type:TEXT, s:"(", height:p.height, depth:p.depth, width:w, small_font:p.small_font, italic_font:0};
-	r = {type:TEXT, s:")", height:p.height, depth:p.depth, width:w, small_font:p.small_font, italic_font:0};
+	l = {type:TEXT, s:"(", height:h, depth:d, width:w, small_font:p.small_font, italic_font:0};
+	r = {type:TEXT, s:")", height:h, depth:d, width:w, small_font:p.small_font, italic_font:0};
 
 	emit_svg_text(l, x, y);
 	emit_svg_text(r, x + p.width - w, y);
