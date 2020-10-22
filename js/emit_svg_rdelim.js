5d9cc6e83c9be0688375eef470a28c0a6ac81b2b
georgeweigt <yo73137@gmail.com>
2020-10-22 11:29:39 -0700

    update

--- js/emit_svg_rdelim.js
+++ js/emit_svg_rdelim.js
@@ -7,15 +7,15 @@ emit_svg_rdelim(p, x, y)
 		t = 1;
 		w = SMALL_DELIM_WIDTH;
 	} else {
-		t = 2;
+		t = 2.5;
 		w = DELIM_WIDTH;
 	}
 
-	var x1 = Math.round(x + p.width - w / 2);
-	var x2 = x1 - w / 2;
+	var x1 = x + p.width - w / 2;
+	var x2 = x + p.width - w;
 
-	var y1 = Math.round(y - p.height + t);
-	var y2 = Math.round(y + p.depth - t);
+	var y1 = y - p.height + t;
+	var y2 = y + p.depth - t;
 
 	emit_svg_line(x1, y1, x1, y2, t); // stem
 	emit_svg_line(x1, y1, x2, y1, t); // top segment
