432f2f140292cf835cbf2db693e6df664575b6c4
georgeweigt <gweigt@icloud.com>
2020-10-24 11:08:29 -0700

    Update draw_vars.js

--- js/draw_vars.js
+++ js/draw_vars.js
@@ -4,8 +4,8 @@ const DRAW_HEIGHT = 300;
 const DRAW_LEFT_PAD = 200;
 const DRAW_RIGHT_PAD = 100;
 
-const DRAW_TOP_PAD = 10;
-const DRAW_BOTTOM_PAD = 30;
+const DRAW_TOP_PAD = 20;
+const DRAW_BOTTOM_PAD = 40;
 
 var xmin;
 var xmax;
