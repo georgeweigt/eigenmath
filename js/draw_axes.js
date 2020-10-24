e2cd9ea27229dd69a67880c876fe67efdd9ce853
georgeweigt <yo73137@gmail.com>
2020-10-24 07:09:54 -0700

    Update draw_axes.js

--- js/draw_axes.js
+++ js/draw_axes.js
@@ -10,8 +10,8 @@ draw_axes()
 	dy = DRAW_HEIGHT - DRAW_HEIGHT * (y - ymin) / (ymax - ymin);
 
 	if (dx > 0 && dx < DRAW_WIDTH)
-		draw_line(dx, 0, dx, DRAW_HEIGHT, 0.5);
+		draw_line(dx, 0, dx, DRAW_HEIGHT, 0.5); // vertical axis
 
 	if (dy > 0 && dy < DRAW_HEIGHT)
-		draw_line(0, dy, DRAW_WIDTH, dy, 0.5);
+		draw_line(0, dy, DRAW_WIDTH, dy, 0.5); // horizontal axis
 }
