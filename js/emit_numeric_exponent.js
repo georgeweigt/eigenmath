46e371a0d805d955afb22c1464dc96023003855d
georgeweigt <gweigt@icloud.com>
2020-10-27 17:47:50 -0700

    Update emit_numeric_exponent.js

--- js/emit_numeric_exponent.js
+++ js/emit_numeric_exponent.js
@@ -3,14 +3,9 @@ emit_numeric_exponent(u, p, small_font) // p is rational or double, sign is not 
 {
 	var v = {type:SUPERSCRIPT, a:[], small_font:small_font};
 
-	if (isrational(p)) {
+	if (isrational(p))
 		emit_rational(v, p, 1);
-//		emit_roman_text(v, Math.abs(p.a).toFixed(0), 1);
-//		if (p.b != 1) {
-//			emit_roman_text(v, "/", 1);
-//			emit_roman_text(v, p.b.toFixed(0), 1);
-//		}
-	} else
+	else
 		emit_double(v, p, 1);
 
 	emit_update_superscript(v, u.a[u.a.length - 1].height);
