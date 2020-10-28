ade2856d64ad1ef8d4e20195fd7b89d340097fab
georgeweigt <gweigt@icloud.com>
2020-10-28 14:59:22 -0700

    Update emit_symbol_text.js

--- js/emit_symbol_text.js
+++ js/emit_symbol_text.js
@@ -1,11 +1,10 @@
 function
 emit_symbol_text(u, s, small_font)
 {
-	if (s.length == 1) {
-		if (isdigit(s))
-			emit_roman_text(u, s, small_font);
-		else
-			emit_italic_text(u, s, small_font);
-	} else
+	if (isdigit(s))
+		emit_roman_text(u, s, small_font);
+	else if (s.length == 1)
+		emit_italic_text(u, s, small_font);
+	else
 		emit_glyph(u, s, small_font);
 }
