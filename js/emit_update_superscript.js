4fe13d13f8d76515591025d8ba6c8f6554f1c3dd
georgeweigt <gweigt@icloud.com>
2020-11-05 13:02:30 -0700

    Update emit_update_superscript.js

--- js/emit_update_superscript.js
+++ js/emit_update_superscript.js
@@ -1,24 +1,25 @@
 function
 emit_update_superscript(u, v) // u is neighbor
 {
-	var h, n;
+	var h, k;
 
 	emit_update(v);
 
 	// move up
 
-	n = u.a.length;
+	if (u.small_font)
+		h = SMALL_FONT_HEIGHT;
+	else
+		h = FONT_HEIGHT;
 
-	if (n == 0) {
-		if (u.small_font)
-			h = SMALL_FONT_HEIGHT;
-		else
-			h = FONT_HEIGHT;
-	} else {
-		if (u.a[n - 1].type == SUBSCRIPT && n > 1)
-			h = u.a[n - 2].height;
-		else
-			h = u.a[n - 1].height;
+	k = u.a.length;
+
+	while (k) {
+		k = k - 1;
+		if (u.a[k].type == SUBSCRIPT)
+			continue;
+		h = u.a[k].height;
+		break;
 	}
 
 	v.height = h + v.height + v.depth;
