d79fb9356c659b644cd4ff4c7aee0c0bb2371a96
georgeweigt <gweigt@icloud.com>
2020-11-05 10:43:51 -0700

    Update emit_update_superscript.js

--- js/emit_update_superscript.js
+++ js/emit_update_superscript.js
@@ -1,14 +1,10 @@
 function
 emit_update_superscript(u, h) // h is height of neighbor
 {
-	var t;
-
 	emit_update(u);
 
 	// move up
 
-	t = u.height + u.depth;
-
-	u.height = h + t;
+	u.height = h + u.height + u.depth;
 	u.depth = -h - u.depth;
 }
