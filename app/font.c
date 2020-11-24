6539e9bad3d1623d1e37984190edcb571875b3b2
georgeweigt <gweigt@icloud.com>
2020-11-24 09:59:35 -0700

    update

--- app/font.c
+++ app/font.c
@@ -1,17 +1,5 @@
 #include "app.h"
 
-void
-init_font(void)
-{
-	font_ref[DEFAULT_FONT] = CTFontCreateWithName(CFSTR("Courier"), 16.0, NULL);
-
-	font_ref[ROMAN_FONT] = CTFontCreateWithName(CFSTR("Times New Roman"), 20.0, NULL);
-	font_ref[ITALIC_FONT] = CTFontCreateWithName(CFSTR("Times New Roman Italic"), 20.0, NULL);
-
-	font_ref[SMALL_ROMAN_FONT] = CTFontCreateWithName(CFSTR("Times New Roman"), 16.0, NULL);
-	font_ref[SMALL_ITALIC_FONT] = CTFontCreateWithName(CFSTR("Times New Roman Italic"), 16.0, NULL);
-}
-
 CTFontRef
 get_font_ref(int font_num)
 {
