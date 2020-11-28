766d7a4799352d0a41b03c1bd870077db36e70a5
georgeweigt <gweigt@icloud.com>
2020-11-28 11:17:13 -0700

    Update draw_text.c

--- app/draw_text.c
+++ app/draw_text.c
@@ -4,25 +4,8 @@ void
 draw_text(double x, double y, uint8_t *buf, int len)
 {
 	int i;
-	CGPoint p;
-	CTFontRef f;
-	CFStringRef s;
-	CGGlyph g;
-
-	y = document_height - y;
-
-	f = get_font_ref(DEFAULT_FONT);
-
 	for (i = 0; i < len; i++) {
-
-		p.x = x;
-		p.y = y;
-
-		s = get_char_name(buf[i]);
-		g = CTFontGetGlyphWithName(f, s);
-
-		CTFontDrawGlyphs(f, &g, &p, 1, gcontext);
-
-		x += CTFontGetAdvancesForGlyphs(f, kCTFontOrientationHorizontal, &g, NULL, 1);
+		draw_char(x, y, DEFAULT_FONT, buf[i]);
+		x += get_char_width(DEFAULT_FONT, buf[i]);
 	}
 }
