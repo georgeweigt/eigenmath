35102198a4a1c329c88982d0a3c03426ba80d2d4
georgeweigt <gweigt@icloud.com>
2020-12-27 09:11:43 -0700

    update

--- src/simplify.c
+++ src/simplify.c
@@ -13,33 +13,168 @@ simplify(void)
 {
 	save();
 	simplify_nib();
+	simplify_trig();
 	restore();
 }
 
 void
 simplify_nib(void)
 {
-	int i, n;
+	int h;
 
 	p1 = pop();
 
 	if (istensor(p1)) {
-		push(p1);
-		copy_tensor();
+		simplify_tensor();
+		return;
+	}
+
+	if (car(p1) == symbol(ADD)) {
+		// simplify each term
+		h = tos;
+		p1 = cdr(p1);
+		while (iscons(p1)) {
+			push(car(p1));
+			simplify_expr();
+			p1 = cdr(p1);
+		}
+		add_terms(tos - h);
 		p1 = pop();
-		n = p1->u.tensor->nelem;
-		for (i = 0; i < n; i++) {
-			push(p1->u.tensor->elem[i]);
-			simplify();
-			p1->u.tensor->elem[i] = pop();
+		if (car(p1) == symbol(ADD)) {
+			push(p1);
+			simplify_expr(); // try rationalizing
+			p1 = pop();
 		}
 		push(p1);
 		return;
 	}
 
+	// p1 is a term (factor or product of factors)
+
 	push(p1);
-	rationalize();
-	eval(); // to normalize
+	simplify_expr();
+	p1 = pop();
+	push(p1);
+}
+
+void
+simplify_tensor(void)
+{
+	int i, n;
+	push(p1);
+	copy_tensor();
+	p1 = pop();
+	n = p1->u.tensor->nelem;
+	for (i = 0; i < n; i++) {
+		push(p1->u.tensor->elem[i]);
+		simplify();
+		p1->u.tensor->elem[i] = pop();
+	}
+	push(p1);
+}
+
+void
+simplify_expr(void)
+{
+	save();
+	simplify_expr_nib();
+	restore();
+}
+
+#undef NUM
+#undef DEN
+#undef R
+#undef T
+
+#define NUM p2
+#define DEN p3
+#define R p4
+#define T p5
+
+void
+simplify_expr_nib(void)
+{
+	p1 = pop();
+
+	if (car(p1) == symbol(ADD)) {
+		push(p1);
+		rationalize();
+		T = pop();
+		if (car(T) == symbol(ADD)) {
+			push(p1); // no change
+			return;
+		}
+	} else
+		T = p1;
+
+	push(T);
+	numerator();
+	NUM = pop();
+
+	push(T);
+	denominator();
+	eval(); // to expand denominator
+	DEN = pop();
+
+	// if DEN is a sum then rationalize it
+
+	if (car(DEN) == symbol(ADD)) {
+		push(DEN);
+		rationalize();
+		T = pop();
+		if (car(T) != symbol(ADD)) {
+			// update NUM
+			push(T);
+			denominator();
+			eval(); // to expand denominator
+			push(NUM);
+			multiply();
+			NUM = pop();
+			// update DEN
+			push(T);
+			numerator();
+			DEN = pop();
+		}
+	}
+
+	// are NUM and DEN congruent sums?
+
+	if (car(NUM) != symbol(ADD) || car(DEN) != symbol(ADD) || length(NUM) != length(DEN)) {
+		// no, but NUM over DEN might be simpler than p1
+		push(NUM);
+		push(DEN);
+		divide();
+		T = pop();
+		if (complexity(T) < complexity(p1))
+			p1 = T;
+		push(p1);
+		return;
+	}
+
+	push(cadr(NUM)); // push first term of numerator
+	push(cadr(DEN)); // push first term of denominator
+	divide();
+
+	R = pop(); // provisional ratio
+
+	push(R);
+	push(DEN);
+	multiply();
+
+	push(NUM);
+	subtract();
+
+	T = pop();
+
+	if (equaln(T, 0))
+		p1 = R;
+
+	push(p1);
+}
+
+void
+simplify_trig(void)
+{
 	p1 = pop();
 
 	push(p1);
