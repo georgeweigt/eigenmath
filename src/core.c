13d29e9f81404185e770f5755763d6fd68581763
georgeweigt <gweigt@icloud.com>
2021-02-15 18:28:39 -0700

    update

--- src/core.c
+++ src/core.c
@@ -764,17 +764,6 @@ count_denominators(struct atom *p)
 }
 
 int
-isdenominator(struct atom *p)
-{
-	if (car(p) == symbol(POWER) && isnegativenumber(caddr(p)))
-		return 1;
-	else if (isrational(p) && !MEQUAL(p->u.q.b, 1))
-		return 1;
-	else
-		return 0;
-}
-
-int
 count_numerators(struct atom *p)
 {
 	int n = 0;
@@ -785,6 +774,17 @@ count_numerators(struct atom *p)
 		p = cdr(p);
 	}
 	return n;
+}
+
+int
+isdenominator(struct atom *p)
+{
+	if (car(p) == symbol(POWER) && isnegativenumber(caddr(p)))
+		return 1;
+	else if (isrational(p) && !MEQUAL(p->u.q.b, 1))
+		return 1;
+	else
+		return 0;
 }
 
 int
