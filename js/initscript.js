f180276d03dd12c0d59fdb59982111781be0ba5e
georgeweigt <gweigt@icloud.com>
2021-02-25 12:47:53 -0700

    Update initscript.js

--- js/initscript.js
+++ js/initscript.js
@@ -4,6 +4,10 @@ var init_script = [
 "cross(u,v) = dot(u,(((0,0,0),(0,0,-1),(0,1,0)),((0,0,1),(0,0,0),(-1,0,0)),((0,-1,0),(1,0,0),(0,0,0))),v)",
 "curl(u) = (d(u[3],y) - d(u[2],z),d(u[1],z) - d(u[3],x),d(u[2],x) - d(u[1],y))",
 "div(u) = d(u[1],x) + d(u[2],y) + d(u[3],z)",
+"taylor(f,x,n,a) = sum(k,0,n,eval(d(f,x,k),x,a) (x - a)^k / k!)",
+"laguerre(x,n,m) = (n + m)! sum(k,0,n,(-x)^k / ((n - k)! (m + k)! k!))",
+"legendre(f,n,m,x) = eval(1 / (2^n n!) (1 - x^2)^(m/2) d((x^2 - 1)^n,x,n + m),x,f)",
+"hermite(x,n) = (-1)^n exp(x^2) d(exp(-x^2),x,n)",
 ];
 
 function
