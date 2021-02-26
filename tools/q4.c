7422146b89c02271a6041b2f9e22eedb0f3c8e24
georgeweigt <gweigt@icloud.com>
2021-02-26 15:42:16 -0700

    Update q4.c

--- tools/q4.c
+++ tools/q4.c
@@ -1,8 +1,9 @@
-// generate hadamard and cnot operators for 4 qbits
+// generate hadamard, not, and cnot operators for 4 qbits
 
 #include <stdio.h>
 
 void hadamard(int t);
+void not(int t);
 void cnot(int c, int t);
 
 int
@@ -14,6 +15,11 @@ main()
 	hadamard(1);
 	hadamard(2);
 	hadamard(3);
+
+	not(0);
+	not(1);
+	not(2);
+	not(3);
 
 	for (i = 0; i < 4; i++)
 		for (j = 0; j < 4; j++)
@@ -53,6 +59,39 @@ hadamard(int t)
 			printf(",\n");
 		else
 			printf(") / sqrt(2)\n\n");
+
+		k++;
+	}
+}
+
+void
+not(int t)
+{
+	int k = 0, q[4], r[4];
+
+	printf("X%d = (", t);
+
+	for (q[0] = 0; q[0] < 2; q[0]++)
+	for (q[1] = 0; q[1] < 2; q[1]++)
+	for (q[2] = 0; q[2] < 2; q[2]++)
+	for (q[3] = 0; q[3] < 2; q[3]++) {
+
+		r[0] = q[0];
+		r[1] = q[1];
+		r[2] = q[2];
+		r[3] = q[3];
+
+		r[t] = q[t] ? 0 : 1; // flip target
+
+		if (k > 0)
+			printf("      ");
+
+		printf("ket%d%d%d%d", r[0], r[1], r[2], r[3]);
+
+		if (k < 15)
+			printf(",\n");
+		else
+			printf(")\n\n");
 
 		k++;
 	}
