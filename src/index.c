9f76c54b3208f180a192550667e8d3de013c74ca
georgeweigt <gweigt@icloud.com>
2021-02-27 12:24:40 -0700

    update

--- src/index.c
+++ src/index.c
@@ -3,82 +3,65 @@
 void
 eval_index(void)
 {
-	int h = tos;
-	p1 = cdr(p1);
+	int k;
+
+	push(cadr(p1));
+	eval();
+
+	p1 = cddr(p1);
+
 	while (iscons(p1)) {
 		push(car(p1));
 		eval();
+		k = pop_integer();
+		indexf(k);
 		p1 = cdr(p1);
 	}
-	index_function(tos - h);
 }
 
-// tensor and indices on stack
-
 void
-index_function(int n)
+indexf(int k)
 {
 	save();
-	index_function_nib(n);
+	indexf_nib(k);
 	restore();
 }
 
 void
-index_function_nib(int n)
+indexf_nib(int k)
 {
-	int i, k, m, ndim, nelem, t;
-	struct atom **s;
+	int i, n;
 
-	s = stack + tos - n;
-	p1 = s[0];
+	p1 = pop();
 
 	if (!istensor(p1)) {
-		tos -= n;
 		push(p1); // quirky, but EVA2.txt depends on it
 		return;
 	}
 
-	ndim = p1->u.tensor->ndim;
+	if (k < 1 || k > p1->u.tensor->dim[0])
+		stop("index error");
 
-	m = n - 1;
+	k--; // make zero based
 
-	if (m > ndim)
-		stop("too many indices for tensor");
+	n = p1->u.tensor->nelem / p1->u.tensor->dim[0];
 
-	k = 0;
-
-	for (i = 0; i < m; i++) {
-		push(s[i + 1]);
-		t = pop_integer();
-		if (t < 1 || t > p1->u.tensor->dim[i])
-			stop("index out of range");
-		k = k * p1->u.tensor->dim[i] + t - 1;
-	}
-
-	if (ndim == m) {
-		tos -= n;
+	if (n == 1) {
 		push(p1->u.tensor->elem[k]);
 		return;
 	}
 
-	for (i = m; i < ndim; i++)
-		k = k * p1->u.tensor->dim[i] + 0;
+	p2 = alloc_tensor(n);
 
-	nelem = 1;
+	for (i = 0; i < n; i++)
+		p2->u.tensor->elem[i] = p1->u.tensor->elem[k * n + i];
 
-	for (i = m; i < ndim; i++)
-		nelem *= p1->u.tensor->dim[i];
+	n = p1->u.tensor->ndim;
 
-	p2 = alloc_tensor(nelem);
+	for (i = 1; i < n; i++)
+		p2->u.tensor->dim[i - 1] = p1->u.tensor->dim[i];
 
-	p2->u.tensor->ndim = ndim - m;
+	p2->u.tensor->ndim = n - 1;
 
-	for (i = m; i < ndim; i++)
-		p2->u.tensor->dim[i - m] = p1->u.tensor->dim[i];
-
-	for (i = 0; i < nelem; i++)
-		p2->u.tensor->elem[i] = p1->u.tensor->elem[k + i];
-
-	tos -= n;
 	push(p2);
 }
