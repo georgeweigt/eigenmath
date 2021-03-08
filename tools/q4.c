f097c3769d5f2b2c74093ecf4037dcc6d4d253f5
georgeweigt <gweigt@icloud.com>
2021-03-08 14:38:20 -0700

    Update q4.c

--- tools/q4.c
+++ tools/q4.c
@@ -1,11 +1,15 @@
-// generate hadamard, not, and cnot operators for 4 qbits
+// generate operators for 4 qbits
 
 #include <stdio.h>
 
 void kets(void);
-void hadamard(int t);
-void not(int t);
-void cnot(int c, int t);
+void X(int t);
+void Z(int t);
+void H(int t);
+void C(int c, int t);
+
+int q[4];
+int r[4];
 
 int
 main()
@@ -14,26 +18,24 @@ main()
 
 	kets();
 
-	hadamard(0);
-	hadamard(1);
-	hadamard(2);
-	hadamard(3);
-
-	not(0);
-	not(1);
-	not(2);
-	not(3);
+	for (i = 0; i < 4; i++) {
+		X(i);
+		Z(i);
+		H(i);
+	}
 
 	for (i = 0; i < 4; i++)
 		for (j = 0; j < 4; j++)
 			if (i != j)
-				cnot(i, j);
+				C(i, j);
+
+	puts("M(psi) = do(xrange = (0,16), yrange = (0,1), draw(psi[ceiling(x)] conj(psi[ceiling(x)]),x))");
 }
 
 void
 kets(void)
 {
-	int i, k, q[4];
+	int i, k;
 
 	for (k = 0; k < 16; k++) {
 
@@ -59,9 +61,71 @@ kets(void)
 }
 
 void
-hadamard(int t)
+X(int t)
 {
-	int k, q[4], r[4];
+	int k;
+
+	printf("X%d = ", t);
+
+	for (k = 0; k < 16; k++) {
+
+		q[0] = (k & 8) ? 1 : 0;
+		q[1] = (k & 4) ? 1 : 0;
+		q[2] = (k & 2) ? 1 : 0;
+		q[3] = (k & 1) ? 1 : 0;
+
+		r[0] = q[0];
+		r[1] = q[1];
+		r[2] = q[2];
+		r[3] = q[3];
+
+		r[t] = q[t] ? 0 : 1; // flip target
+
+		if (k > 0)
+			printf("     ");
+
+		printf("outer(ket%d%d%d%d,ket%d%d%d%d)", q[0], q[1], q[2], q[3], r[0], r[1], r[2], r[3]);
+
+		if (k < 15)
+			printf(" +\n");
+		else
+			printf("\n\n");
+	}
+}
+
+void
+Z(int t)
+{
+	int k;
+
+	printf("Z%d = ", t);
+
+	for (k = 0; k < 16; k++) {
+
+		q[0] = (k & 8) ? 1 : 0;
+		q[1] = (k & 4) ? 1 : 0;
+		q[2] = (k & 2) ? 1 : 0;
+		q[3] = (k & 1) ? 1 : 0;
+
+		if (k > 0)
+			printf("     ");
+
+		if (q[t] == 1)
+			printf("outer(ket%d%d%d%d,-ket%d%d%d%d)", q[0], q[1], q[2], q[3], q[0], q[1], q[2], q[3]);
+		else
+			printf("outer(ket%d%d%d%d,ket%d%d%d%d)", q[0], q[1], q[2], q[3], q[0], q[1], q[2], q[3]);
+
+		if (k < 15)
+			printf(" +\n");
+		else
+			printf("\n\n");
+	}
+}
+
+void
+H(int t)
+{
+	int k;
 
 	printf("H%d = ", t);
 
@@ -99,42 +163,9 @@ hadamard(int t)
 }
 
 void
-not(int t)
+C(int c, int t)
 {
-	int k, q[4], r[4];
-
-	printf("X%d = ", t);
-
-	for (k = 0; k < 16; k++) {
-
-		q[0] = (k & 8) ? 1 : 0;
-		q[1] = (k & 4) ? 1 : 0;
-		q[2] = (k & 2) ? 1 : 0;
-		q[3] = (k & 1) ? 1 : 0;
-
-		r[0] = q[0];
-		r[1] = q[1];
-		r[2] = q[2];
-		r[3] = q[3];
-
-		r[t] = q[t] ? 0 : 1; // flip target
-
-		if (k > 0)
-			printf("     ");
-
-		printf("outer(ket%d%d%d%d,ket%d%d%d%d)", q[0], q[1], q[2], q[3], r[0], r[1], r[2], r[3]);
-
-		if (k < 15)
-			printf(" +\n");
-		else
-			printf("\n\n");
-	}
-}
-
-void
-cnot(int c, int t)
-{
-	int k, q[4], r[4];
+	int k;
 
 	printf("C%d%d = ", c, t);
 
