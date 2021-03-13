4a14c61826c452711dada7c1257143e211f335fd
georgeweigt <gweigt@icloud.com>
2021-03-13 08:53:45 -0700

    update

--- js/abs.js
+++ js/abs.js
@@ -1,13 +1,13 @@
 function
 abs()
 {
-	var h, p1;
+	var p1;
 
 	p1 = pop();
 
 	if (istensor(p1)) {
 		if (p1.dim.length != 1)
-			stopf("abs: tensor rank > 1");
+			stopf("abs");
 		push(p1);
 		push(p1);
 		conj();
@@ -17,54 +17,10 @@ abs()
 		return;
 	}
 
-	if (isnum(p1)) {
-		push(p1);
-		if (isnegativenumber(p1))
-			negate();
-		return;
-	}
-
-	if (iscomplexnumber(p1)) {
-		push(p1);
-		push(p1);
-		conj();
-		multiply();
-		push_rational(1, 2);
-		power();
-		return;
-	}
-
-	// abs(1/a) evaluates to 1/abs(a)
-
-	if (car(p1) == symbol(POWER) && isnegativeterm(caddr(p1))) {
-		push(p1);
-		reciprocate();
-		abs();
-		reciprocate();
-		return;
-	}
-
-	// abs(a*b) evaluates to abs(a)*abs(b)
-
-	if (car(p1) == symbol(MULTIPLY)) {
-		h = stack.length;
-		p1 = cdr(p1);
-		while (iscons(p1)) {
-			push(car(p1));
-			abs();
-			p1 = cdr(p1);
-		}
-		multiply_factors(stack.length - h);
-		return;
-	}
-
-	if (isnegativeterm(p1) || (car(p1) == symbol(ADD) && isnegativeterm(cadr(p1)))) {
-		push(p1);
-		negate();
-		p1 = pop();
-	}
-
-	push_symbol(ABS);
 	push(p1);
-	list(2);
+	push(p1);
+	conj();
+	multiply();
+	push_rational(1, 2);
+	power();
 }
