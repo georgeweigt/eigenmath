c1b027d76be73c8ce154915383db471384d189cb
georgeweigt <gweigt@icloud.com>
2021-03-23 12:38:31 -0700

    update

--- js/cos.js
+++ js/cos.js
@@ -68,23 +68,40 @@ cos()
 		return;
 	}
 
-	// multiply by 180/pi
+	// n pi ?
 
-	push(p1); // nonnegative by code above
-	push_integer(180);
-	multiply();
+	push(p1);
 	push_symbol(PI);
 	divide();
-	p2 = pop()
+	p2 = pop();
 
-	if (isrational(p2))
-		n = p2.a / p2.b;
-	else if (isdouble(p2))
-		n = p2.d;
-	else
-		n = 1; // force default case
+	if (!isnum(p2)) {
+		push_symbol(COS);
+		push(p1);
+		list(2);
+		return;
+	}
 
-	switch (n % 360) {
+	if (isdouble(p2)) {
+		push_double(Math.cos(p2.d * Math.PI));
+		return;
+	}
+
+	push(p2);
+	push_integer(180);
+	multiply();
+	p2 = pop();
+
+	if (!isinteger(p2)) {
+		push_symbol(COS);
+		push(p1);
+		list(2);
+		return;
+	}
+
+	n = p2.a % 360;
+
+	switch (n) {
 	case 90:
 	case 270:
 		push_integer(0);
