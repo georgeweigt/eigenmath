1e49263c512bd6833c1d5862642d7b627191d580
georgeweigt <gweigt@icloud.com>
2021-03-25 20:33:09 -0700

    update

--- js/eval_prefixform.js
+++ js/eval_prefixform.js
@@ -1,10 +1,10 @@
 function
 eval_prefixform(p1)
 {
-	var s;
 	push(cadr(p1));
 	evalf();
 	p1 = pop();
-	s = prefixform(p1);
-	push_string(s);
+	outbuf = "";
+	prefixform(p1);
+	push_string(outbuf);
 }
--- js/prefixform.js
+++ js/prefixform.js
@@ -2,35 +2,27 @@ function
 prefixform(p)
 {
 	if (iscons(p)) {
-		var s = "(";
-		s += prefixform(car(p));
+		outbuf += "(";
+		prefixform(car(p));
 		p = cdr(p);
 		while (iscons(p)) {
-			s += " ";
-			s += prefixform(car(p));
+			outbuf += " ";
+			prefixform(car(p));
 			p = cdr(p);
 		}
-		s += ")";
-		return s;
-	}
-
-	if (isinteger(p))
-		return p.a.toFixed(0);
-
-	if (isrational(p))
-		return p.a.toFixed(0) + "/" + p.b.toFixed(0);
-
-	if (isdouble(p))
-		return p.d.toPrecision(6);
-
-	if (issymbol(p))
-		return "" + p.printname;
-
-	if (isstring(p))
-		return "'" + p.string + "'";
-
-	if (istensor(p))
-		return "(tensor)";
-
-	return "?";
+		outbuf += ")";
+	} else if (isinteger(p))
+		outbuf += p.a.toFixed(0);
+	else if (isrational(p))
+		outbuf += p.a.toFixed(0) + "/" + p.b.toFixed(0);
+	else if (isdouble(p))
+		outbuf += p.d.toPrecision(6);
+	else if (issymbol(p))
+		outbuf += p.printname;
+	else if (isstring(p))
+		outbuf += "'" + p.string + "'";
+	else if (istensor(p))
+		outbuf += "[ ]";
+	else
+		outbuf += " ? ";
 }
