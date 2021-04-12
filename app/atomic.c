b8fb770337167fac80bd93287c34e1f359db4ca9
georgeweigt <gweigt@icloud.com>
2021-04-12 08:29:29 -0700

    Update atomic.c

--- app/atomic.c
+++ app/atomic.c
@@ -1,7 +1,9 @@
 #include "app.h"
 #include <stdatomic.h>
 
-// atomic ops needed for memory barriers
+// atomic ops needed for memory barrier
+
+// memory barrier needed because cpu reorders memory writes
 
 _Atomic(struct display *) display_ptr;
 
