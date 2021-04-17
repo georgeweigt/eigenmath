cad573715f0147bda884ba9776955b115e04642c
georgeweigt <gweigt@icloud.com>
2021-04-17 13:44:57 -0700

    update

--- js/eval_rotate.js
+++ js/eval_rotate.js
@@ -11,14 +11,14 @@ eval_rotate(p1)
 	psi = pop();
 
 	if (!istensor(psi) || psi.dim.length > 1 || psi.elem.length > 32768 || (psi.elem.length & (psi.elem.length - 1)) != 0)
-		stopf("rotate error 1");
+		stopf("rotate error 1 first argument is not a vector or dimension error");
 
 	p1 = cddr(p1);
 
 	while (iscons(p1)) {
 
 		if (!iscons(cdr(p1)))
-			stopf("rotate error 2");
+			stopf("rotate error 2 unexpected end of argument list");
 
 		opcode = car(p1);
 		push(cadr(p1));
@@ -27,19 +27,19 @@ eval_rotate(p1)
 		c = n; // control bit
 
 		if (n > 14 || (1 << n) >= psi.elem.length)
-			stopf("rotate error 3");
+			stopf("rotate error 3 qubit number format or range");
 
 		p1 = cddr(p1);
 
 		if (opcode == symbol("C")) {
 			if (!iscons(cdr(p1)))
-				stopf("rotate error 2");
+				stopf("rotate error 2 unexpected end of argument list");
 			opcode = car(p1);
 			push(cadr(p1));
 			evalf();
 			n = pop_integer();
 			if (n > 14 || (1 << n) >= psi.elem.length)
-				stopf("rotate error 3");
+				stopf("rotate error 3 qubit number format or range");
 			p1 = cddr(p1);
 		}
 
@@ -50,7 +50,7 @@ eval_rotate(p1)
 
 		if (opcode == symbol("P")) {
 			if (!iscons(p1))
-				stopf("rotate error 2");
+				stopf("rotate error 2 unexpected end of argument list");
 			push(car(p1));
 			p1 = cdr(p1);
 			evalf();
@@ -75,13 +75,13 @@ eval_rotate(p1)
 		if (opcode == symbol("W")) {
 			m = n;
 			if (!iscons(p1))
-				stopf("rotate error 2");
+				stopf("rotate error 2 unexpected end of argument list");
 			push(car(p1));
 			p1 = cdr(p1);
 			evalf();
 			n = pop_integer();
 			if (n > 14 || (1 << n) >= psi.elem.length)
-				stopf("rotate error 3");
+				stopf("rotate error 3 qubit number format or range");
 			rotate_w(psi, c, m, n);
 			continue;
 		}
@@ -101,7 +101,7 @@ eval_rotate(p1)
 			continue;
 		}
 
-		stopf("rotate error 4");
+		stopf("rotate error 4 unknown rotation code");
 	}
 
 	push(psi);
