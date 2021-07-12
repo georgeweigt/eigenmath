e4d2dbbd1970802ebbf513ddfa3ca9125d827b80
georgeweigt <gweigt@icloud.com>
2021-07-12 07:59:09 -0700

    update

--- js/divisor.js
+++ js/divisor.js
@@ -13,3 +13,45 @@ divisor(p)
 
 	return divisor_term(p);
 }
+
+function
+divisor_term(p)
+{
+	if (car(p) == symbol(MULTIPLY)) {
+		p = cdr(p);
+		while (iscons(p)) {
+			if (divisor_factor(car(p)))
+				return 1;
+			p = cdr(p);
+		}
+		return 0;
+	}
+
+	return divisor_factor(p);
+}
+
+function
+divisor_factor(p)
+{
+	if (isrational(p)) {
+		if (isinteger(p))
+			return 0;
+		push_integer(p.b);
+		return 1;
+	}
+
+	if (car(p) == symbol(POWER) && !isminusone(cadr(p)) && isnegativeterm(caddr(p))) {
+		if (isminusone(caddr(p)))
+			push(cadr(p));
+		else {
+			push_symbol(POWER);
+			push(cadr(p));
+			push(caddr(p));
+			negate();
+			list(3);
+		}
+		return 1;
+	}
+
+	return 0;
+}
