498993b6c51ab44c8440f2242b66636980ac3fce
georgeweigt <gweigt@icloud.com>
2021-08-24 17:13:07 -0700

    update

--- js/integral_tables.js
+++ js/integral_tables.js
@@ -260,6 +260,10 @@ var integral_tab_trig = [
 	"-cos(a x)^5 / (5 a) + 2 cos(a x)^3 / (3 a) - cos(a x) / a",
 	"1",
 
+	"sin(a x)^6",
+	"sin(2 a x)^3 / (48 a) - sin(2 a x) / (4 a) + 3 sin(4 a x) / (64 a) + 5/16 x",
+	"1",
+
 // cos(a x)^n
 
 	"cos(a x)^2",
@@ -276,6 +280,10 @@ var integral_tab_trig = [
 
 	"cos(a x)^5",
 	"sin(a x)^5 / (5 a) - 2 sin(a x)^3 / (3 a) + sin(a x) / a",
+	"1",
+
+	"cos(a x)^6",
+	"-sin(2 a x)^3 / (48 a) + sin(2 a x) / (4 a) + 3 sin(4 a x) / (64 a) + 5/16 x",
 	"1",
 
 //
@@ -340,14 +348,10 @@ var integral_tab_trig = [
 	"-1 / (a sin(a x))",
 	"1",
 
-	"cos(a x)^2 / sin(a x)",
-	"cos(a x) / a + log(sin(a x / 2)) / a - log(cos(a x / 2)) / a",
-	"1",
-
 	"cos(a x)^2 / sin(a x)^2",
 	"-x - cos(a x) / sin(a x) / a",
 	"1",
-//
+
 	"sin(a + b x)",
 	"-cos(a + b x) / b",
 	"1",
@@ -476,20 +480,22 @@ var integral_tab_trig = [
 	"-cos(a x)^5 / (5 a)",
 	"1",
 
-	"cos(a x)^6",
-	"-sin(2 a x)^3 / (48 a) + sin(2 a x) / (4 a) + 3 sin(4 a x) / (64 a) + 5/16 x",
+	"cos(a x)^7 / sin(a x)^2",
+	"-sin(a x)^5 / (5 a) + sin(a x)^3 / a - 3 sin(a x) / a - 1 / (a sin(a x))",
 	"1",
 
-	"cos(a x)^6 / sin(a x)",
-	"cos(a x)^5 / (5 a) + cos(a x)^3 / (3 a) + cos(a x) / a + log(-cos(a x) + 1) / (2 a) - log(cos(a x) + 1) / (2 a)",
+// cos(a x)^n / sin(a x)
+
+	"cos(a x)^2 / sin(a x)",
+	"cos(a x) / a + log(tan(1/2 a x)) / a",
 	"1",
 
 	"cos(a x)^4 / sin(a x)",
-	"cos(a x)^3 / (3 a) + cos(a x) / a + log(-cos(a x) + 1) / (2 a) - log(cos(a x) + 1) / (2 a)",
+	"4 cos(a x) / (3 a) - cos(a x) sin(a x)^2 / (3 a) + log(tan(1/2 a x)) / a",
 	"1",
 
-	"cos(a x)^7 / sin(a x)^2",
-	"-sin(a x)^5 / (5 a) + sin(a x)^3 / a - 3 sin(a x) / a - 1 / (a sin(a x))",
+	"cos(a x)^6 / sin(a x)",
+	"cos(a x)^5 / (5 a) - 2 cos(a x)^3 / (3 a) + 2 cos(a x) / a - cos(a x) sin(a x)^2 / a + log(tan(1/2 a x)) / a",
 	"1",
 ];
 
