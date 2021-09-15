2ed2a64b8897494d21c51ee817e10932f615971a
georgeweigt <gweigt@icloud.com>
2021-09-15 14:15:57 -0700

    update

--- js/bignum_mul.js
+++ js/bignum_mul.js
@@ -1,7 +1,7 @@
 function
 bignum_mul(u, v)
 {
-	var i, j, nu, nv, t, w = [];
+	var i, j, nu, nv, nw, t, w = [];
 
 	nu = u.length;
 	nv = v.length;
@@ -15,13 +15,15 @@ bignum_mul(u, v)
 		return w;
 	}
 
-	for (i = 0; i < nu; i++)
+	nw = nu + nv;
+
+	for (i = 0; i < nw; i++)
 		w[i] = 0;
 
 	for (j = 0; j < nv; j++) {
 		t = 0;
 		for (i = 0; i < nu; i++) {
-			t = t + u[i] * v[j] + w[i + j];
+			t += u[i] * v[j] + w[i + j];
 			w[i + j] = t % BIGM;
 			t = Math.floor(t / BIGM);
 		}
