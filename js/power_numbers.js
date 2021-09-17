9515e3864544fc44820c793b958a497be6078bf3
georgeweigt <gweigt@icloud.com>
2021-09-17 13:12:09 -0700

    Update power_numbers.js

--- js/power_numbers.js
+++ js/power_numbers.js
@@ -3,8 +3,6 @@
 function
 power_numbers(BASE, EXPO)
 {
-	var base, expo;
-
 	if (iszero(EXPO)) {
 		push_integer(1); // 0^0 = 1
 		return;
@@ -32,11 +30,6 @@ power_numbers(BASE, EXPO)
 		return;
 	}
 
-
-
-	if (iszero(BASE) && isnegativenumber(EXPO))
-		stopf("divide by zero");
-
 	if (isinteger(EXPO)) {
 		power_simple(BASE, EXPO);
 		return;
@@ -52,23 +45,12 @@ power_numbers(BASE, EXPO)
 		push(BASE);
 		negate();
 		BASE = pop();
-		power_numbers(BASE, EXPO);
+		power_rationals(BASE, EXPO);
 		multiply();
 		return;
 	}
 
-	if (isrational(BASE) && isrational(EXPO)) {
-		power_rationals(BASE, EXPO);
-		return;
-	}
-
-	push(BASE);
-	base = pop_double();
-
-	push(EXPO);
-	expo = pop_double();
-
-	push_double(Math.pow(base, expo));
+	power_rationals(BASE, EXPO);
 }
 
 function
