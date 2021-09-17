b6c5c6f56eb3444b97b0d4273b9caef60da8a4e3
georgeweigt <gweigt@icloud.com>
2021-09-17 09:43:40 -0700

    update

--- js/multiply_numbers.js
+++ js/multiply_numbers.js
@@ -16,3 +16,30 @@ multiply_numbers(p1, p2)
 
 	push_double(a * b);
 }
+
+function
+multiply_rationals(p1, p2)
+{
+	var a, b, c;
+
+	a = p1.a * p2.a;
+	b = p1.b * p2.b;
+
+	if (a == 0) {
+		push_integer(0);
+		return;
+	}
+
+	if (Math.abs(a) > MAXINT || b > MAXINT) {
+		push_double(a / b);
+		return;
+	}
+
+	if (b > 1) {
+		c = gcd_integers(a, b)
+		a /= c;
+		b /= c;
+	}
+
+	push_rational(a, b);
+}
