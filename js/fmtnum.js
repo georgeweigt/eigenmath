363511904f788c3b1383d4fa5804f880e999ca4c
georgeweigt <gweigt@icloud.com>
2021-10-02 07:40:33 -0700

    update

--- js/fmtnum.js
+++ js/fmtnum.js
@@ -1,7 +1,7 @@
 function
 fmtnum(n)
 {
-	if (Math.abs(n) < 0.0001)
+	if (Math.abs(n) > 0 && Math.abs(n) < 0.0001)
 		return Math.abs(n).toExponential(5);
 	else
 		return Math.abs(n).toPrecision(6);
