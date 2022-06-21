51b843c1d3395ec99eebe5043e153f18f3b8b54d
georgeweigt <gweigt@icloud.com>
2022-06-20 20:25:58 -0700

    update

--- eigenmath.c
+++ eigenmath.c
@@ -595,13 +595,6 @@ void expcos(void);
 void eval_expsin(struct atom *p1);
 void expsin(void);
 void eval_factor(struct atom *p1);
-void factor_again(void);
-void factor_term(void);
-void factor_rational(void);
-void factor_factor(void);
-void factor_bignum(uint32_t *a, struct atom *EXPO);
-void eval_factorial(struct atom *p1);
-void factorial(void);
 void factorpoly(void);
 void factorpoly_coeffs(struct atom *P, struct atom *X);
 int factorpoly_root(int h);
@@ -610,6 +603,10 @@ void factorpoly_eval(int h, int n, struct atom *X);
 void factorpoly_push_divisors(int a);
 void factorpoly_gen(int h, int k);
 void factorpoly_factor_small_number(int n);
+void factor_factor(void);
+void factor_bignum(uint32_t *a, struct atom *EXPO);
+void eval_factorial(struct atom *p1);
+void factorial(void);
 int factors(struct atom *p);
 void push_term_factors(struct atom *p);
 void eval_filter(struct atom *p1);
@@ -6470,271 +6467,15 @@ expsin(void)
 void
 eval_factor(struct atom *p1)
 {
-	struct atom *p2;
 	push(cadr(p1));
 	eval();
-	p2 = pop();
-	if (isrational(p2)) {
-		push(p2);
-		factor_rational();
-		return;
-	}
-	if (isdouble(p2)) {
-		convert_double_to_rational(p2->u.d);
-		factor_rational();
-		return;
-	}
-	push(p2);
-	push(caddr(p1));
-	eval();
-	p2 = pop();
-	if (p2 == symbol(NIL))
-		push_symbol(X_LOWER);
-	else
-		push(p2);
-	factorpoly();
-	// more factoring?
-	p1 = cdddr(p1);
-	while (iscons(p1)) {
+	p1 = cddr(p1);
+	if (iscons(p1)) {
 		push(car(p1));
 		eval();
-		factor_again();
-		p1 = cdr(p1);
-	}
-}
-
-void
-factor_again(void)
-{
-	int h, n;
-	struct atom *p1, *p2;
-	p2 = pop();
-	p1 = pop();
-	h = tos;
-	if (car(p1) == symbol(MULTIPLY)) {
-		p1 = cdr(p1);
-		while (iscons(p1)) {
-			push(car(p1));
-			push(p2);
-			factor_term();
-			p1 = cdr(p1);
-		}
-	} else {
-		push(p1);
-		push(p2);
-		factor_term();
-	}
-	n = tos - h;
-	if (n > 1)
-		multiply_factors_noexpand(n);
-}
-
-void
-factor_term(void)
-{
-	struct atom *p1;
-	factorpoly();
-	p1 = pop();
-	if (car(p1) == symbol(MULTIPLY)) {
-		p1 = cdr(p1);
-		while (iscons(p1)) {
-			push(car(p1));
-			p1 = cdr(p1);
-		}
 	} else
-		push(p1);
-}
-
-void
-factor_rational(void)
-{
-	int h, i, n, t;
-	struct atom *p1, *p2;
-	p1 = pop();
-	h = tos;
-	// factor numerator
-	push(p1);
-	numerator();
-	p2 = pop();
-	if (!isplusone(p2)) {
-		push(p2);
-		factor_number();
-	}
-	// factor denominator
-	push(p1);
-	denominator();
-	p2 = pop();
-	if (!isplusone(p2)) {
-		t = tos;
-		push(p2);
-		factor_number();
-		for (i = t; i < tos; i++) {
-			p2 = stack[i];
-			if (car(p2) == symbol(POWER)) {
-				push_symbol(POWER);
-				push(cadr(p2)); // base
-				push(caddr(p2)); // exponent
-				negate();
-				list(3);
-			} else {
-				push_symbol(POWER);
-				push(p2); // base
-				push_integer(-1); // exponent
-				list(3);
-			}
-			stack[i] = pop();
-		}
-	}
-	n = tos - h;
-	if (n == 0)
-		push(one);
-	else if (n > 1) {
-		list(n);
-		push_symbol(MULTIPLY);
-		swap();
-		cons(); // make MULTIPLY head of list
-	}
-}
-
-// factors N or N^M where N and M are rational numbers, returns factors on stack
-
-void
-factor_factor(void)
-{
-	uint32_t *numer, *denom;
-	struct atom *FARG, *BASE, *EXPO;
-	FARG = pop();
-	if (car(FARG) == symbol(POWER)) {
-		BASE = cadr(FARG);
-		EXPO = caddr(FARG);
-		if (BASE->k != RATIONAL || EXPO->k != RATIONAL) {
-			push(FARG);
-			return;
-		}
-		if (isminusone(BASE)) {
-			push(FARG); // -1 to the M
-			return;
-		}
-		if (BASE->sign == MMINUS) {
-			push_symbol(POWER);
-			push_integer(-1);
-			push(EXPO);
-			list(3); // leave on stack
-		}
-		numer = BASE->u.q.a;
-		denom = BASE->u.q.b;
-		if (!MEQUAL(numer, 1))
-			factor_bignum(numer, EXPO);
-		if (!MEQUAL(denom, 1)) {
-			// flip sign of exponent
-			push(EXPO);
-			negate();
-			EXPO = pop();
-			factor_bignum(denom, EXPO);
-		}
-		return;
-	}
-	if (FARG->k != RATIONAL || iszero(FARG) || isplusone(FARG) || isminusone(FARG)) {
-		push(FARG);
-		return;
-	}
-	if (FARG->sign == MMINUS)
-		push_integer(-1);
-	numer = FARG->u.q.a;
-	denom = FARG->u.q.b;
-	if (!MEQUAL(numer, 1)) {
-		EXPO = one;
-		factor_bignum(numer, EXPO);
-	}
-	if (!MEQUAL(denom, 1)) {
-		EXPO = minusone;
-		factor_bignum(denom, EXPO);
-	}
-}
-
-void
-factor_bignum(uint32_t *a, struct atom *EXPO)
-{
-	int k, m;
-	uint32_t d, n;
-	struct atom *P;
-	if (MLENGTH(a) > 1) {
-		// too big to factor
-		push_bignum(MPLUS, mcopy(a), mint(1));
-		if (!isplusone(EXPO)) {
-			push_symbol(POWER);
-			swap();
-			push(EXPO);
-			list(3);
-		}
-		return;
-	}
-	n = a[0];
-	for (k = 0; k < MAXPRIMETAB; k++) {
-		d = primetab[k];
-		if (n / d < d)
-			break; // n is 1 or prime
-		m = 0;
-		while (n % d == 0) {
-			n /= d;
-			m++;
-		}
-		if (m == 0)
-			continue;
-		push_bignum(MPLUS, mint(d), mint(1));
-		push_integer(m);
-		push(EXPO);
-		multiply();
-		P = pop();
-		if (!isplusone(P)) {
-			push_symbol(POWER);
-			swap();
-			push(P);
-			list(3);
-		}
-	}
-	if (n > 1) {
-		push_bignum(MPLUS, mint(n), mint(1));
-		if (!isplusone(EXPO)) {
-			push_symbol(POWER);
-			swap();
-			push(EXPO);
-			list(3);
-		}
-	}
-}
-
-void
-eval_factorial(struct atom *p1)
-{
-	push(cadr(p1));
-	eval();
-	factorial();
-}
-
-void
-factorial(void)
-{
-	int n;
-	struct atom *p1;
-	p1 = pop();
-	if (!issmallinteger(p1)) {
-		push_symbol(FACTORIAL);
-		push(p1);
-		list(2);
-		return;
-	}
-	push(p1);
-	n = pop_integer();
-	if (n < 0) {
-		push_symbol(FACTORIAL);
-		push(p1);
-		list(2);
-		return;
-	}
-	bignum_factorial(n);
-	if (isdouble(p1))
-		bignum_float();
+		push_symbol(X_LOWER);
+	factorpoly();
 }
 
 void
@@ -6927,9 +6668,9 @@ factorpoly_push_divisors(int a)
 	tos = h + n;
 }
 
-//	Generate divisors
+//	Generate all divisors for a factored number
 //
-//	Input:		Base-exponent pairs on stack
+//	Input:		Factor pairs on stack (base, expo)
 //
 //			h	first pair
 //
@@ -6937,33 +6678,26 @@ factorpoly_push_divisors(int a)
 //
 //	Output:		Divisors on stack
 //
-//	For example, factor list 2 2 3 1 results in 6 divisors,
+//	For example, the number 12 (= 2^2 3^1) has 6 divisors:
 //
-//		1
-//		3
-//		2
-//		6
-//		4
-//		12
+//	1, 2, 3, 4, 6, 12
 
 void
 factorpoly_gen(int h, int k)
 {
-	int expo, i;
+	int i, n;
 	struct atom *ACCUM, *BASE, *EXPO;
-	ACCUM = pop();
-	if (h == k) {
-		push(ACCUM);
+	if (h == k)
 		return;
-	}
+	ACCUM = pop();
 	BASE = stack[h + 0];
 	EXPO = stack[h + 1];
 	push(EXPO);
-	expo = pop_integer();
-	for (i = 0; i <= abs(expo); i++) {
+	n = pop_integer();
+	for (i = 0; i <= n; i++) {
 		push(ACCUM);
 		push(BASE);
-		push_integer(sign(expo) * i);
+		push_integer(i);
 		power();
 		multiply();
 		factorpoly_gen(h + 2, k);
@@ -6996,6 +6730,147 @@ factorpoly_factor_small_number(int n)
 		push_integer(n); // push pair
 		push_integer(1);
 	}
+}
+
+// factors N or N^M where N and M are rational numbers, returns factors on stack
+
+void
+factor_factor(void)
+{
+	uint32_t *numer, *denom;
+	struct atom *FARG, *BASE, *EXPO;
+	FARG = pop();
+	if (car(FARG) == symbol(POWER)) {
+		BASE = cadr(FARG);
+		EXPO = caddr(FARG);
+		if (BASE->k != RATIONAL || EXPO->k != RATIONAL) {
+			push(FARG);
+			return;
+		}
+		if (isminusone(BASE)) {
+			push(FARG); // -1 to the M
+			return;
+		}
+		if (BASE->sign == MMINUS) {
+			push_symbol(POWER);
+			push_integer(-1);
+			push(EXPO);
+			list(3); // leave on stack
+		}
+		numer = BASE->u.q.a;
+		denom = BASE->u.q.b;
+		if (!MEQUAL(numer, 1))
+			factor_bignum(numer, EXPO);
+		if (!MEQUAL(denom, 1)) {
+			// flip sign of exponent
+			push(EXPO);
+			negate();
+			EXPO = pop();
+			factor_bignum(denom, EXPO);
+		}
+		return;
+	}
+	if (FARG->k != RATIONAL || iszero(FARG) || isplusone(FARG) || isminusone(FARG)) {
+		push(FARG);
+		return;
+	}
+	if (FARG->sign == MMINUS)
+		push_integer(-1);
+	numer = FARG->u.q.a;
+	denom = FARG->u.q.b;
+	if (!MEQUAL(numer, 1)) {
+		EXPO = one;
+		factor_bignum(numer, EXPO);
+	}
+	if (!MEQUAL(denom, 1)) {
+		EXPO = minusone;
+		factor_bignum(denom, EXPO);
+	}
+}
+
+void
+factor_bignum(uint32_t *a, struct atom *EXPO)
+{
+	int k, m;
+	uint32_t d, n;
+	struct atom *P;
+	if (MLENGTH(a) > 1) {
+		// too big to factor
+		push_bignum(MPLUS, mcopy(a), mint(1));
+		if (!isplusone(EXPO)) {
+			push_symbol(POWER);
+			swap();
+			push(EXPO);
+			list(3);
+		}
+		return;
+	}
+	n = a[0];
+	for (k = 0; k < MAXPRIMETAB; k++) {
+		d = primetab[k];
+		if (n / d < d)
+			break; // n is 1 or prime
+		m = 0;
+		while (n % d == 0) {
+			n /= d;
+			m++;
+		}
+		if (m == 0)
+			continue;
+		push_bignum(MPLUS, mint(d), mint(1));
+		push_integer(m);
+		push(EXPO);
+		multiply();
+		P = pop();
+		if (!isplusone(P)) {
+			push_symbol(POWER);
+			swap();
+			push(P);
+			list(3);
+		}
+	}
+	if (n > 1) {
+		push_bignum(MPLUS, mint(n), mint(1));
+		if (!isplusone(EXPO)) {
+			push_symbol(POWER);
+			swap();
+			push(EXPO);
+			list(3);
+		}
+	}
+}
+
+void
+eval_factorial(struct atom *p1)
+{
+	push(cadr(p1));
+	eval();
+	factorial();
+}
+
+void
+factorial(void)
+{
+	int n;
+	struct atom *p1;
+	p1 = pop();
+	if (!issmallinteger(p1)) {
+		push_symbol(FACTORIAL);
+		push(p1);
+		list(2);
+		return;
+	}
+	push(p1);
+	n = pop_integer();
+	if (n < 0) {
+		push_symbol(FACTORIAL);
+		push(p1);
+		list(2);
+		return;
+	}
+	bignum_factorial(n);
+	if (isdouble(p1))
+		bignum_float();
 }
 
 // Push expression factors onto the stack. For example...
--- src/factor.c
+++ src/factor.c
@@ -3,153 +3,348 @@
 void
 eval_factor(struct atom *p1)
 {
-	struct atom *p2;
-
 	push(cadr(p1));
 	eval();
 
-	p2 = pop();
+	p1 = cddr(p1);
 
-	if (isrational(p2)) {
-		push(p2);
-		factor_rational();
-		return;
-	}
-
-	if (isdouble(p2)) {
-		convert_double_to_rational(p2->u.d);
-		factor_rational();
-		return;
-	}
-
-	push(p2);
-
-	push(caddr(p1));
-	eval();
-
-	p2 = pop();
-
-	if (p2 == symbol(NIL))
-		push_symbol(X_LOWER);
-	else
-		push(p2);
-
-	factorpoly();
-
-	// more factoring?
-
-	p1 = cdddr(p1);
-	while (iscons(p1)) {
+	if (iscons(p1)) {
 		push(car(p1));
 		eval();
-		factor_again();
-		p1 = cdr(p1);
-	}
-}
-
-void
-factor_again(void)
-{
-	int h, n;
-	struct atom *p1, *p2;
-
-	p2 = pop();
-	p1 = pop();
-
-	h = tos;
-
-	if (car(p1) == symbol(MULTIPLY)) {
-		p1 = cdr(p1);
-		while (iscons(p1)) {
-			push(car(p1));
-			push(p2);
-			factor_term();
-			p1 = cdr(p1);
-		}
-	} else {
-		push(p1);
-		push(p2);
-		factor_term();
-	}
-
-	n = tos - h;
-
-	if (n > 1)
-		multiply_factors_noexpand(n);
-}
-
-void
-factor_term(void)
-{
-	struct atom *p1;
-	factorpoly();
-	p1 = pop();
-	if (car(p1) == symbol(MULTIPLY)) {
-		p1 = cdr(p1);
-		while (iscons(p1)) {
-			push(car(p1));
-			p1 = cdr(p1);
-		}
 	} else
-		push(p1);
+		push_symbol(X_LOWER);
+
+	factorpoly();
 }
 
 void
-factor_rational(void)
+factorpoly(void)
 {
-	int h, i, n, t;
-	struct atom *p1, *p2;
+	int h, i, n;
+	struct atom *C, *F, *P, *R, *X;
 
-	p1 = pop();
+	X = pop();
+	P = pop();
 
 	h = tos;
 
-	// factor numerator
+	factorpoly_coeffs(P, X); // put coeffs on stack
 
-	push(p1);
-	numerator();
-	p2 = pop();
-	if (!isplusone(p2)) {
-		push(p2);
-		factor_number();
-	}
+	F = one;
 
-	// factor denominator
+	while (tos - h > 1) {
 
-	push(p1);
-	denominator();
-	p2 = pop();
-	if (!isplusone(p2)) {
-		t = tos;
-		push(p2);
-		factor_number();
-		for (i = t; i < tos; i++) {
-			p2 = stack[i];
-			if (car(p2) == symbol(POWER)) {
-				push_symbol(POWER);
-				push(cadr(p2)); // base
-				push(caddr(p2)); // exponent
-				negate();
-				list(3);
-			} else {
-				push_symbol(POWER);
-				push(p2); // base
-				push_integer(-1); // exponent
-				list(3);
-			}
+		C = pop(); // leading coeff
+
+		if (iszero(C))
+			continue;
+
+		push(F);
+		push(C);
+		multiply_noexpand();
+		F = pop();
+
+		// divide through by C
+
+		for (i = h; i < tos; i++) {
+			push(stack[i]);
+			push(C);
+			divide();
 			stack[i] = pop();
 		}
+
+		push_integer(1); // leading coeff
+
+		if (factorpoly_root(h) == 0)
+			break;
+
+		R = pop();
+
+		push(F);
+		push(X);
+		push(R);
+		subtract();
+		multiply_noexpand();
+		F = pop();
+
+		factorpoly_divide(h, R);
+
+		pop(); // remove leading coeff
 	}
 
 	n = tos - h;
 
-	if (n == 0)
-		push(one);
-	else if (n > 1) {
-		list(n);
-		push_symbol(MULTIPLY);
-		swap();
-		cons(); // make MULTIPLY head of list
+	if (n == 0) {
+		push(F);
+		return;
+	}
+
+	// remainder
+
+	for (i = 0; i < n; i++) {
+		push(stack[h + i]);
+		push(X);
+		push_integer(i);
+		power();
+		multiply();
+		stack[h + i] = pop();
+	}
+
+	add_terms(n);
+
+	push(F);
+	multiply_noexpand();
+}
+
+void
+factorpoly_coeffs(struct atom *P, struct atom *X)
+{
+	int h;
+	struct atom *C;
+
+	h = tos;
+
+	for (;;) {
+
+		push(P);
+		push(X);
+		push_integer(0);
+		subst();
+		eval();
+		C = pop();
+
+		push(C);
+
+		push(P);
+		push(C);
+		subtract();
+		P = pop();
+
+		if (iszero(P))
+			break;
+
+		push(P);
+		push(X);
+		divide();
+		P = pop();
+	}
+}
+
+int
+factorpoly_root(int h)
+{
+	int a, i, j, h1, h2, n, n1, n2;
+	struct atom *C, *T, *X;
+
+	n = tos - h;
+
+	C = stack[h]; // constant term
+
+	if (!isrational(C))
+		stop("factor");
+
+	if (iszero(C)) {
+		push(C);
+		return 1;
+	}
+
+	h1 = tos;
+	push(C);
+	numerator();
+	a = pop_integer();
+	factorpoly_push_divisors(a);
+	n1 = tos - h1;
+
+	h2 = tos;
+	push(C);
+	denominator();
+	a = pop_integer();
+	factorpoly_push_divisors(a);
+	n2 = tos - h2;
+
+	for (i = 0; i < n1; i++) {
+		for (j = 0; j < n2; j++) {
+
+			push(stack[h1 + i]);
+			push(stack[h2 + j]);
+			divide();
+			X = pop();
+
+			factorpoly_eval(h, n, X);
+
+			T = pop();
+
+			if (iszero(T)) {
+				tos = h + n; // pop all
+				push(X);
+				return 1;
+			}
+
+			push(X);
+			negate();
+			X = pop();
+
+			factorpoly_eval(h, n, X);
+
+			T = pop();
+
+			if (iszero(T)) {
+				tos = h + n; // pop all
+				push(X);
+				return 1;
+			}
+		}
+	}
+
+	tos = h + n; // pop all
+
+	return 0; // no root
+}
+
+// divide by X - R where R is a root
+
+void
+factorpoly_divide(int h, struct atom *R)
+{
+	int i;
+	struct atom *C;
+
+	C = one;
+
+	for (i = tos - 2; i > h; i--) {
+
+		push(stack[i]);
+		push(C);
+		push(R);
+		multiply();
+		add();
+
+		stack[i] = C;
+
+		C = pop();
+	}
+
+	stack[h] = C;
+}
+
+// evaluate p(x) at x = X using horner's rule
+
+void
+factorpoly_eval(int h, int n, struct atom *X)
+{
+	int i;
+
+	push(stack[h + n - 1]);
+
+	for (i = n - 1; i > 0; i--) {
+		push(X);
+		multiply();
+		push(stack[h + i - 1]);
+		add();
+	}
+}
+
+void
+factorpoly_push_divisors(int a)
+{
+	int h, i, k, n;
+
+	h = tos;
+
+	factorpoly_factor_small_number(a);
+
+	k = tos;
+
+	// contruct divisors by recursive descent
+
+	push_integer(1);
+
+	factorpoly_gen(h, k);
+
+	// move
+
+	n = tos - k;
+
+	for (i = 0; i < n; i++)
+		stack[h + i] = stack[k + i];
+
+	tos = h + n;
+}
+
+//	Generate all divisors for a factored number
+//
+//	Input:		Factor pairs on stack (base, expo)
+//
+//			h	first pair
+//
+//			k	just past last pair
+//
+//	Output:		Divisors on stack
+//
+//	For example, the number 12 (= 2^2 3^1) has 6 divisors:
+//
+//	1, 2, 3, 4, 6, 12
+
+void
+factorpoly_gen(int h, int k)
+{
+	int i, n;
+	struct atom *ACCUM, *BASE, *EXPO;
+
+	if (h == k)
+		return;
+
+	ACCUM = pop();
+
+	BASE = stack[h + 0];
+	EXPO = stack[h + 1];
+
+	push(EXPO);
+	n = pop_integer();
+
+	for (i = 0; i <= n; i++) {
+		push(ACCUM);
+		push(BASE);
+		push_integer(i);
+		power();
+		multiply();
+		factorpoly_gen(h + 2, k);
+	}
+}
+
+// n is 32 bits or less
+
+void
+factorpoly_factor_small_number(int n)
+{
+	int d, k, m;
+
+	if (n < 0)
+		n = -n;
+
+	for (k = 0; k < MAXPRIMETAB; k++) {
+
+		d = primetab[k];
+
+		if (n / d < d)
+			break; // n is 1 or prime
+
+		m = 0;
+
+		while (n % d == 0) {
+			n /= d;
+			m++;
+		}
+
+		if (m) {
+			push_integer(d); // push pair
+			push_integer(m);
+		}
+	}
+
+	if (n > 1) {
+		push_integer(n); // push pair
+		push_integer(1);
 	}
 }
 
