9877be9c51d5d8bd76036285ba6abe3611982d75
georgeweigt <gweigt@icloud.com>
2022-07-25 21:40:17 -0700

    update

--- eigenmath.c
+++ eigenmath.c
@@ -552,8 +552,6 @@ void d_scalar_tensor(struct atom *p1, struct atom *p2);
 void d_tensor_scalar(struct atom *p1, struct atom *p2);
 void eval_det(struct atom *p1);
 void det(void);
-void divisors(int n);
-void divisors_nib(int h, int k);
 void eval_eigenvec(struct atom *p1);
 void eigenvec(double *D, double *Q, int n);
 int eigenvec_step(double *D, double *Q, int n);
@@ -593,8 +591,6 @@ void eval_filter(struct atom *p1);
 void filter(void);
 void filter_sum(struct atom *p1, struct atom *p2);
 void filter_tensor(struct atom *p1, struct atom *p2);
-int findroot(int h);
-void findroot_eval(int h, int n, struct atom *X);
 void eval_float(struct atom *p1);
 void floatfunc(void);
 void floatfunc_subst(void);
@@ -858,9 +854,13 @@ void eval_real(struct atom *p1);
 void real(void);
 void eval_rect(struct atom *p1);
 void rect(void);
-void reduce(int h, struct atom *A);
 void eval_roots(struct atom *p1);
 void roots(void);
+int findroot(int h);
+void findroot_eval(int h, int n, struct atom *X);
+void reduce(int h, struct atom *A);
+void divisors(int n);
+void divisors_nib(int h, int k);
 void eval_rotate(struct atom *p1);
 void rotate_h(struct atom *PSI, uint32_t c, int n);
 void rotate_p(struct atom *PSI, struct atom *PHASE, uint32_t c, int n);
@@ -5901,75 +5901,6 @@ det(void)
 	else
 		add_terms(tos - h);
 }
-// push all divisors of n
-
-void
-divisors(int n)
-{
-	int h, i, k;
-
-	h = tos;
-
-	factor_int(n);
-
-	k = tos;
-
-	// contruct divisors by recursive descent
-
-	push_integer(1);
-
-	divisors_nib(h, k);
-
-	// move
-
-	n = tos - k;
-
-	for (i = 0; i < n; i++)
-		stack[h + i] = stack[k + i];
-
-	tos = h + n; // pop all
-}
-
-//	Generate all divisors for a factored number
-//
-//	Input:		Factor pairs on stack (base, expo)
-//
-//			h	first pair
-//
-//			k	just past last pair
-//
-//	Output:		Divisors on stack
-//
-//	For example, the number 12 (= 2^2 3^1) has 6 divisors:
-//
-//	1, 2, 3, 4, 6, 12
-
-void
-divisors_nib(int h, int k)
-{
-	int i, n;
-	struct atom *ACCUM, *BASE, *EXPO;
-
-	if (h == k)
-		return;
-
-	ACCUM = pop();
-
-	BASE = stack[h + 0];
-	EXPO = stack[h + 1];
-
-	push(EXPO);
-	n = pop_integer();
-
-	for (i = 0; i <= n; i++) {
-		push(ACCUM);
-		push(BASE);
-		push_integer(i);
-		power();
-		multiply();
-		divisors_nib(h + 2, k);
-	}
-}
 void
 eval_eigenvec(struct atom *p1)
 {
@@ -7710,88 +7641,6 @@ filter_tensor(struct atom *p1, struct atom *p2)
 		p3->u.tensor->elem[i] = pop();
 	}
 	push(p3);
-}
-// polynomial coefficients are on the stack
-
-int
-findroot(int h)
-{
-	int i, j, k, m, n;
-	struct atom *C, *T, *X;
-
-	C = stack[h]; // constant term
-
-	if (iszero(C)) {
-		push_integer(0); // root is zero
-		return 1;
-	}
-
-	k = tos;
-	push(C);
-	numerator();
-	n = pop_integer();
-	divisors(n); // push divisors of n
-
-	m = tos;
-	push(C);
-	denominator();
-	n = pop_integer();
-	divisors(n); // push divisors of n
-
-	for (i = k; i < m; i++) {
-		for (j = m; j < tos; j++) {
-
-			push(stack[i]);
-			push(stack[j]);
-			divide();
-			X = pop();
-
-			findroot_eval(h, k - h, X);
-
-			T = pop();
-
-			if (iszero(T)) {
-				tos = k; // pop all
-				push(X);
-				return 1;
-			}
-
-			push(X);
-			negate();
-			X = pop();
-
-			findroot_eval(h, k - h, X);
-
-			T = pop();
-
-			if (iszero(T)) {
-				tos = k; // pop all
-				push(X);
-				return 1;
-			}
-		}
-	}
-
-	tos = k; // pop all
-
-	return 0; // no root
-}
-
-// evaluate p(x) at x = X using horner's rule
-
-void
-findroot_eval(int h, int n, struct atom *X)
-{
-	int i;
-
-	push(stack[h + n - 1]);
-
-	for (i = n - 1; i > 0; i--) {
-		push(X);
-		multiply();
-		push(stack[h + i - 1]);
-		add();
-	}
 }
 void
 eval_float(struct atom *p1)
@@ -16992,32 +16841,6 @@ rect(void)
 
 	multiply();
 }
-// divide by X - A
-
-void
-reduce(int h, struct atom *A)
-{
-	int i, t;
-
-	t = tos - 1;
-
-	for (i = t; i > h; i--) {
-		push(A);
-		push(stack[i]);
-		multiply();
-		push(stack[i - 1]);
-		add();
-		stack[i - 1] = pop();
-	}
-
-	if (!iszero(stack[h]))
-		stop("roots: residual error");
-
-	for (i = h; i < t; i++)
-		stack[i] = stack[i + 1];
-
-	pop(); // one less coeff on stack
-}
 void
 eval_roots(struct atom *p1)
 {
@@ -17131,6 +16954,186 @@ roots(void)
 	tos = h; // pop all
 
 	push(A);
+}
+
+// polynomial coefficients are on the stack
+
+int
+findroot(int h)
+{
+	int i, j, k, m, n;
+	struct atom *C, *T, *X;
+
+	C = stack[h]; // constant term
+
+	if (iszero(C)) {
+		push_integer(0); // root is zero
+		return 1;
+	}
+
+	k = tos;
+	push(C);
+	numerator();
+	n = pop_integer();
+	divisors(n); // push divisors of n
+
+	m = tos;
+	push(C);
+	denominator();
+	n = pop_integer();
+	divisors(n); // push divisors of n
+
+	for (i = k; i < m; i++) {
+		for (j = m; j < tos; j++) {
+
+			push(stack[i]);
+			push(stack[j]);
+			divide();
+			X = pop();
+
+			findroot_eval(h, k - h, X);
+
+			T = pop();
+
+			if (iszero(T)) {
+				tos = k; // pop all
+				push(X);
+				return 1;
+			}
+
+			push(X);
+			negate();
+			X = pop();
+
+			findroot_eval(h, k - h, X);
+
+			T = pop();
+
+			if (iszero(T)) {
+				tos = k; // pop all
+				push(X);
+				return 1;
+			}
+		}
+	}
+
+	tos = k; // pop all
+
+	return 0; // no root
+}
+
+// evaluate p(x) at x = X using horner's rule
+
+void
+findroot_eval(int h, int n, struct atom *X)
+{
+	int i;
+
+	push(stack[h + n - 1]);
+
+	for (i = n - 1; i > 0; i--) {
+		push(X);
+		multiply();
+		push(stack[h + i - 1]);
+		add();
+	}
+}
+
+// divide by X - A
+
+void
+reduce(int h, struct atom *A)
+{
+	int i, t;
+
+	t = tos - 1;
+
+	for (i = t; i > h; i--) {
+		push(A);
+		push(stack[i]);
+		multiply();
+		push(stack[i - 1]);
+		add();
+		stack[i - 1] = pop();
+	}
+
+	if (!iszero(stack[h]))
+		stop("roots: residual error");
+
+	for (i = h; i < t; i++)
+		stack[i] = stack[i + 1];
+
+	pop(); // one less coeff on stack
+}
+
+// push all divisors of n
+
+void
+divisors(int n)
+{
+	int h, i, k;
+
+	h = tos;
+
+	factor_int(n);
+
+	k = tos;
+
+	// contruct divisors by recursive descent
+
+	push_integer(1);
+
+	divisors_nib(h, k);
+
+	// move
+
+	n = tos - k;
+
+	for (i = 0; i < n; i++)
+		stack[h + i] = stack[k + i];
+
+	tos = h + n; // pop all
+}
+
+//	Generate all divisors for a factored number
+//
+//	Input:		Factor pairs on stack (base, expo)
+//
+//			h	first pair
+//
+//			k	just past last pair
+//
+//	Output:		Divisors on stack
+//
+//	For example, the number 12 (= 2^2 3^1) has 6 divisors:
+//
+//	1, 2, 3, 4, 6, 12
+
+void
+divisors_nib(int h, int k)
+{
+	int i, n;
+	struct atom *ACCUM, *BASE, *EXPO;
+
+	if (h == k)
+		return;
+
+	ACCUM = pop();
+
+	BASE = stack[h + 0];
+	EXPO = stack[h + 1];
+
+	push(EXPO);
+	n = pop_integer();
+
+	for (i = 0; i <= n; i++) {
+		push(ACCUM);
+		push(BASE);
+		push_integer(i);
+		power();
+		multiply();
+		divisors_nib(h + 2, k);
+	}
 }
 #define NUMQBITS PSI->u.tensor->nelem
 #define KET0 PSI->u.tensor->elem[i ^ n]
--- xcode/src/C/eigenmath.c
+++ xcode/src/C/eigenmath.c
@@ -4927,75 +4927,6 @@ det(void)
 	else
 		add_terms(tos - h);
 }
-// push all divisors of n
-
-void
-divisors(int n)
-{
-	int h, i, k;
-
-	h = tos;
-
-	factor_int(n);
-
-	k = tos;
-
-	// contruct divisors by recursive descent
-
-	push_integer(1);
-
-	divisors_nib(h, k);
-
-	// move
-
-	n = tos - k;
-
-	for (i = 0; i < n; i++)
-		stack[h + i] = stack[k + i];
-
-	tos = h + n; // pop all
-}
-
-//	Generate all divisors for a factored number
-//
-//	Input:		Factor pairs on stack (base, expo)
-//
-//			h	first pair
-//
-//			k	just past last pair
-//
-//	Output:		Divisors on stack
-//
-//	For example, the number 12 (= 2^2 3^1) has 6 divisors:
-//
-//	1, 2, 3, 4, 6, 12
-
-void
-divisors_nib(int h, int k)
-{
-	int i, n;
-	struct atom *ACCUM, *BASE, *EXPO;
-
-	if (h == k)
-		return;
-
-	ACCUM = pop();
-
-	BASE = stack[h + 0];
-	EXPO = stack[h + 1];
-
-	push(EXPO);
-	n = pop_integer();
-
-	for (i = 0; i <= n; i++) {
-		push(ACCUM);
-		push(BASE);
-		push_integer(i);
-		power();
-		multiply();
-		divisors_nib(h + 2, k);
-	}
-}
 void
 eval_eigenvec(struct atom *p1)
 {
@@ -6736,88 +6667,6 @@ filter_tensor(struct atom *p1, struct atom *p2)
 		p3->u.tensor->elem[i] = pop();
 	}
 	push(p3);
-}
-// polynomial coefficients are on the stack
-
-int
-findroot(int h)
-{
-	int i, j, k, m, n;
-	struct atom *C, *T, *X;
-
-	C = stack[h]; // constant term
-
-	if (iszero(C)) {
-		push_integer(0); // root is zero
-		return 1;
-	}
-
-	k = tos;
-	push(C);
-	numerator();
-	n = pop_integer();
-	divisors(n); // push divisors of n
-
-	m = tos;
-	push(C);
-	denominator();
-	n = pop_integer();
-	divisors(n); // push divisors of n
-
-	for (i = k; i < m; i++) {
-		for (j = m; j < tos; j++) {
-
-			push(stack[i]);
-			push(stack[j]);
-			divide();
-			X = pop();
-
-			findroot_eval(h, k - h, X);
-
-			T = pop();
-
-			if (iszero(T)) {
-				tos = k; // pop all
-				push(X);
-				return 1;
-			}
-
-			push(X);
-			negate();
-			X = pop();
-
-			findroot_eval(h, k - h, X);
-
-			T = pop();
-
-			if (iszero(T)) {
-				tos = k; // pop all
-				push(X);
-				return 1;
-			}
-		}
-	}
-
-	tos = k; // pop all
-
-	return 0; // no root
-}
-
-// evaluate p(x) at x = X using horner's rule
-
-void
-findroot_eval(int h, int n, struct atom *X)
-{
-	int i;
-
-	push(stack[h + n - 1]);
-
-	for (i = n - 1; i > 0; i--) {
-		push(X);
-		multiply();
-		push(stack[h + i - 1]);
-		add();
-	}
 }
 void
 eval_float(struct atom *p1)
@@ -14287,32 +14136,6 @@ rect(void)
 
 	multiply();
 }
-// divide by X - A
-
-void
-reduce(int h, struct atom *A)
-{
-	int i, t;
-
-	t = tos - 1;
-
-	for (i = t; i > h; i--) {
-		push(A);
-		push(stack[i]);
-		multiply();
-		push(stack[i - 1]);
-		add();
-		stack[i - 1] = pop();
-	}
-
-	if (!iszero(stack[h]))
-		stop("roots: residual error");
-
-	for (i = h; i < t; i++)
-		stack[i] = stack[i + 1];
-
-	pop(); // one less coeff on stack
-}
 void
 eval_roots(struct atom *p1)
 {
@@ -14426,6 +14249,186 @@ roots(void)
 	tos = h; // pop all
 
 	push(A);
+}
+
+// polynomial coefficients are on the stack
+
+int
+findroot(int h)
+{
+	int i, j, k, m, n;
+	struct atom *C, *T, *X;
+
+	C = stack[h]; // constant term
+
+	if (iszero(C)) {
+		push_integer(0); // root is zero
+		return 1;
+	}
+
+	k = tos;
+	push(C);
+	numerator();
+	n = pop_integer();
+	divisors(n); // push divisors of n
+
+	m = tos;
+	push(C);
+	denominator();
+	n = pop_integer();
+	divisors(n); // push divisors of n
+
+	for (i = k; i < m; i++) {
+		for (j = m; j < tos; j++) {
+
+			push(stack[i]);
+			push(stack[j]);
+			divide();
+			X = pop();
+
+			findroot_eval(h, k - h, X);
+
+			T = pop();
+
+			if (iszero(T)) {
+				tos = k; // pop all
+				push(X);
+				return 1;
+			}
+
+			push(X);
+			negate();
+			X = pop();
+
+			findroot_eval(h, k - h, X);
+
+			T = pop();
+
+			if (iszero(T)) {
+				tos = k; // pop all
+				push(X);
+				return 1;
+			}
+		}
+	}
+
+	tos = k; // pop all
+
+	return 0; // no root
+}
+
+// evaluate p(x) at x = X using horner's rule
+
+void
+findroot_eval(int h, int n, struct atom *X)
+{
+	int i;
+
+	push(stack[h + n - 1]);
+
+	for (i = n - 1; i > 0; i--) {
+		push(X);
+		multiply();
+		push(stack[h + i - 1]);
+		add();
+	}
+}
+
+// divide by X - A
+
+void
+reduce(int h, struct atom *A)
+{
+	int i, t;
+
+	t = tos - 1;
+
+	for (i = t; i > h; i--) {
+		push(A);
+		push(stack[i]);
+		multiply();
+		push(stack[i - 1]);
+		add();
+		stack[i - 1] = pop();
+	}
+
+	if (!iszero(stack[h]))
+		stop("roots: residual error");
+
+	for (i = h; i < t; i++)
+		stack[i] = stack[i + 1];
+
+	pop(); // one less coeff on stack
+}
+
+// push all divisors of n
+
+void
+divisors(int n)
+{
+	int h, i, k;
+
+	h = tos;
+
+	factor_int(n);
+
+	k = tos;
+
+	// contruct divisors by recursive descent
+
+	push_integer(1);
+
+	divisors_nib(h, k);
+
+	// move
+
+	n = tos - k;
+
+	for (i = 0; i < n; i++)
+		stack[h + i] = stack[k + i];
+
+	tos = h + n; // pop all
+}
+
+//	Generate all divisors for a factored number
+//
+//	Input:		Factor pairs on stack (base, expo)
+//
+//			h	first pair
+//
+//			k	just past last pair
+//
+//	Output:		Divisors on stack
+//
+//	For example, the number 12 (= 2^2 3^1) has 6 divisors:
+//
+//	1, 2, 3, 4, 6, 12
+
+void
+divisors_nib(int h, int k)
+{
+	int i, n;
+	struct atom *ACCUM, *BASE, *EXPO;
+
+	if (h == k)
+		return;
+
+	ACCUM = pop();
+
+	BASE = stack[h + 0];
+	EXPO = stack[h + 1];
+
+	push(EXPO);
+	n = pop_integer();
+
+	for (i = 0; i <= n; i++) {
+		push(ACCUM);
+		push(BASE);
+		push_integer(i);
+		power();
+		multiply();
+		divisors_nib(h + 2, k);
+	}
 }
 #define NUMQBITS PSI->u.tensor->nelem
 #define KET0 PSI->u.tensor->elem[i ^ n]
