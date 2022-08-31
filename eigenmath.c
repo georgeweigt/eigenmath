a8545ebe176c1b681600e8b041e03446125c53d8
georgeweigt <gweigt@icloud.com>
2022-08-31 16:40:00 -0700

    update

--- eigenmath.c
+++ eigenmath.c
@@ -13051,13 +13051,14 @@ outbuf_init(void)
 void
 outbuf_puts(char *s)
 {
-	int n;
+	int m, n;
 	n = (int) strlen(s);
-	if (outbuf_index + n + 1 > outbuf_length) {
-		outbuf_length = 1000 * ((outbuf_index + n) / 1000 + 1); // length is multiple of 1000
-		outbuf = (char *) realloc(outbuf, outbuf_length);
+	m = 1000 * ((outbuf_index + n) / 1000 + 1); // m is a multiple of 1000
+	if (m > outbuf_length) {
+		outbuf = (char *) realloc(outbuf, m);
 		if (outbuf == NULL)
 			exit(1);
+		outbuf_length = m;
 	}
 	strcpy(outbuf + outbuf_index, s);
 	outbuf_index += n;
@@ -13066,11 +13067,13 @@ outbuf_puts(char *s)
 void
 outbuf_putc(int c)
 {
-	if (outbuf_index + 2 > outbuf_length) {
-		outbuf_length += 1000;
-		outbuf = (char *) realloc(outbuf, outbuf_length);
+	int m;
+	m = 1000 * ((outbuf_index + 1) / 1000 + 1); // m is a multiple of 1000
+	if (m > outbuf_length) {
+		outbuf = (char *) realloc(outbuf, m);
 		if (outbuf == NULL)
 			exit(1);
+		outbuf_length = m;
 	}
 	outbuf[outbuf_index++] = c;
 	outbuf[outbuf_index] = '\0';
