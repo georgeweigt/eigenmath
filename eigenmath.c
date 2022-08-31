821a929dc8bc65cbd1cbb8265f4f1d1a3ae73c14
georgeweigt <gweigt@icloud.com>
2022-08-31 13:55:39 -0700

    update

--- eigenmath.c
+++ eigenmath.c
@@ -13052,7 +13052,7 @@ void
 outbuf_puts(char *s)
 {
 	int n;
-	n = strlen(s);
+	n = (int) strlen(s);
 	if (outbuf_length - outbuf_index < n + 1) {
 		outbuf_length += 1000 + n + 1;
 		outbuf = (char *) realloc(outbuf, outbuf_length);
