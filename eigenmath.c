d8f1f977d2e6a3920bf787f55a787996a5bcffc4
georgeweigt <gweigt@icloud.com>
2022-09-01 11:11:22 -0700

    update

--- eigenmath.c
+++ eigenmath.c
@@ -13050,15 +13050,15 @@ outbuf_init(void)
 void
 outbuf_puts(char *s)
 {
-	int m, n;
+	int len, m;
 
-	n = (int) strlen(s);
+	len = (int) strlen(s);
 
-	// Let outbuf_index + n == 1000
+	// Let outbuf_index + len == 1000
 
 	// Then m == 2000 hence there is always room for the terminator '\0'
 
-	m = 1000 * ((outbuf_index + n) / 1000 + 1); // m is a multiple of 1000
+	m = 1000 * ((outbuf_index + len) / 1000 + 1); // m is a multiple of 1000
 
 	if (m > outbuf_length) {
 		outbuf = realloc(outbuf, m);
@@ -13068,7 +13068,7 @@ outbuf_puts(char *s)
 	}
 
 	strcpy(outbuf + outbuf_index, s);
-	outbuf_index += n;
+	outbuf_index += len;
 }
 
 void
