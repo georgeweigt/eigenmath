f15c8c3b72b3103e3072bab35a21ee0a234b73bf
georgeweigt <gweigt@icloud.com>
2022-11-15 16:38:40 -0700

    Update run_thread.c

--- xcode/src/C/run_thread.c
+++ xcode/src/C/run_thread.c
@@ -1,18 +1,19 @@
 #include "app.h"
 #include <pthread.h>
 
+pthread_t thread_mem;
+
 void
 run_thread(char *s)
 {
-	pthread_t thread;
-
 	if (running)
 		return;
 
 	running = 1;
 
-	pthread_create(&thread, NULL, run_thread_nib, s);
-	pthread_detach(thread);
+	memset(&thread_mem, 0, sizeof thread_mem);
+	pthread_create(&thread_mem, NULL, run_thread_nib, s);
+	pthread_detach(thread_mem);
 }
 
 void *
