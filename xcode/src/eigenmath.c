8ad767edf39519d9365a2be7c853d58257f3c59b
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-03-15 15:18:21 -0700

    update

--- xcode/src/eigenmath.c
+++ xcode/src/eigenmath.c
@@ -17000,29 +17000,44 @@ eval_user_function(struct atom *p1)
 void
 eval_zero(struct atom *p1)
 {
-	int dim[MAXDIM], i, m, n;
-	m = 1;
-	n = 0;
+	int h, i, m, n;
+
 	p1 = cdr(p1);
+	h = tos;
+	m = 1;
+
 	while (iscons(p1)) {
-		if (n == MAXDIM)
-			stopf("zero: rank err");
 		push(car(p1));
 		eval();
-		i = pop_integer();
-		if (i < 2)
+		n = pop_integer();
+		if (n < 2)
 			stopf("zero: dim err");
-		m *= i;
-		dim[n++] = i;
+		m *= n;
+		push_integer(n);
 		p1 = cdr(p1);
 	}
+
+	n = tos - h;
+
 	if (n == 0) {
-		push_integer(0);
+		push_integer(0); // scalar zero
 		return;
 	}
+
+	if (n > MAXDIM)
+		stopf("zero: rank err");
+
 	p1 = alloc_tensor(m);
+
+	for (i = 0; i < m; i++)
+		p1->u.tensor->elem[i] = zero;
+
+	// dim info
+
 	p1->u.tensor->ndim = n;
+
 	for (i = 0; i < n; i++)
-		p1->u.tensor->dim[i] = dim[i];
+		p1->u.tensor->dim[n - i - 1] = pop_integer();
+
 	push(p1);
 }
