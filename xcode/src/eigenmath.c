6535b8b4a95ef95f7b8cf629c2ee4427fa738c81
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-04-19 11:43:41 -0700

    update

--- xcode/src/eigenmath.c
+++ xcode/src/eigenmath.c
@@ -444,9 +444,6 @@ int isdenormalpolar(struct atom *p);
 int isdenormalpolarterm(struct atom *p);
 int issquarematrix(struct atom *p);
 int issmallinteger(struct atom *p);
-void decomp(void);
-void decomp_sum(struct atom *F, struct atom *X);
-void decomp_product(struct atom *F, struct atom *X);
 int equal(struct atom *p1, struct atom *p2);
 void evalg(void);
 void evalf(void);
@@ -612,6 +609,10 @@ void integral_lookup(int h, struct atom *F);
 int integral_classify(struct atom *p);
 int integral_search(int h, struct atom *F, char **table, int n);
 int integral_search_nib(int h, struct atom *F, struct atom *I, struct atom *C);
+void decomp(void);
+void decomp_sum(struct atom *F, struct atom *X);
+void decomp_product(struct atom *F, struct atom *X);
+void partition_term(void);
 void eval_inv(struct atom *p1);
 void inv(void);
 void eval_kronecker(struct atom *p1);
@@ -839,7 +840,6 @@ void eval_exit(struct atom *p1);
 void outbuf_init(void);
 void outbuf_puts(char *s);
 void outbuf_putc(int c);
-void partition_term(void);
 char * read_file(char *filename);
 void run(char *buf);
 void run_buf(char *buf);
@@ -2413,164 +2413,6 @@ issmallinteger(struct atom *p)
 		return p->u.d == floor(p->u.d) && fabs(p->u.d) <= 0x7fffffff;
 
 	return 0;
-}
-// push const coeffs
-
-void
-decomp(void)
-{
-	struct atom *p1, *F, *X;
-
-	X = pop();
-	F = pop();
-
-	// is the entire expression constant?
-
-	if (!findf(F, X)) {
-		push(F);
-		return;
-	}
-
-	// sum?
-
-	if (car(F) == symbol(ADD)) {
-		decomp_sum(F, X);
-		return;
-	}
-
-	// product?
-
-	if (car(F) == symbol(MULTIPLY)) {
-		decomp_product(F, X);
-		return;
-	}
-
-	// naive decomp
-
-	p1 = cdr(F);
-	while (iscons(p1)) {
-		push(car(p1));
-		push(X);
-		decomp();
-		p1 = cdr(p1);
-	}
-}
-
-void
-decomp_sum(struct atom *F, struct atom *X)
-{
-	int h, i, j, k, n;
-	struct atom *p1, *p2;
-
-	h = tos;
-
-	// partition terms
-
-	p1 = cdr(F);
-
-	while (iscons(p1)) {
-		p2 = car(p1);
-		if (findf(p2, X)) {
-			if (car(p2) == symbol(MULTIPLY)) {
-				push(p2);
-				push(X);
-				partition_term();	// push const part then push var part
-			} else {
-				push_integer(1);	// const part
-				push(p2);		// var part
-			}
-		}
-		p1 = cdr(p1);
-	}
-
-	// combine const parts of matching var parts
-
-	n = tos - h;
-
-	for (i = 0; i < n - 2; i += 2)
-		for (j = i + 2; j < n; j += 2) {
-			if (!equal(stack[h + i + 1], stack[h + j + 1]))
-				continue;
-			push(stack[h + i]); // add const parts
-			push(stack[h + j]);
-			add();
-			stack[h + i] = pop();
-			for (k = j; k < n - 2; k++)
-				stack[h + k] = stack[h + k + 2];
-			j -= 2; // use same j again
-			n -= 2;
-			tos -= 2; // pop
-		}
-
-	// push const parts, decomp var parts
-
-	list(tos - h);
-	p1 = pop();
-
-	while (iscons(p1)) {
-		push(car(p1)); // const part
-		push(cadr(p1)); // var part
-		push(X);
-		decomp();
-		p1 = cddr(p1);
-	}
-
-	// add together all constant terms
-
-	h = tos;
-	p1 = cdr(F);
-	while (iscons(p1)) {
-		if (!findf(car(p1), X))
-			push(car(p1));
-		p1 = cdr(p1);
-	}
-
-	n = tos - h;
-
-	if (n > 1) {
-		list(n);
-		push_symbol(ADD);
-		swap();
-		cons(); // makes ADD head of list
-	}
-}
-
-void
-decomp_product(struct atom *F, struct atom *X)
-{
-	int h, n;
-	struct atom *p1;
-
-	// decomp factors involving x
-
-	p1 = cdr(F);
-	while (iscons(p1)) {
-		if (findf(car(p1), X)) {
-			push(car(p1));
-			push(X);
-			decomp();
-		}
-		p1 = cdr(p1);
-	}
-
-	// combine constant factors
-
-	h = tos;
-	p1 = cdr(F);
-	while (iscons(p1)) {
-		if (!findf(car(p1), X))
-			push(car(p1));
-		p1 = cdr(p1);
-	}
-
-	n = tos - h;
-
-	if (n > 1) {
-		list(n);
-		push_symbol(MULTIPLY);
-		swap();
-		cons(); // makes MULTIPLY head of list
-	}
 }
 int
 equal(struct atom *p1, struct atom *p2)
@@ -8726,6 +8568,217 @@ integral_search_nib(int h, struct atom *F, struct atom *I, struct atom *C)
 	}
 
 	return 0;					// no
+}
+
+// push const coeffs
+
+void
+decomp(void)
+{
+	struct atom *p1, *F, *X;
+
+	X = pop();
+	F = pop();
+
+	// is the entire expression constant?
+
+	if (!findf(F, X)) {
+		push(F);
+		return;
+	}
+
+	// sum?
+
+	if (car(F) == symbol(ADD)) {
+		decomp_sum(F, X);
+		return;
+	}
+
+	// product?
+
+	if (car(F) == symbol(MULTIPLY)) {
+		decomp_product(F, X);
+		return;
+	}
+
+	// naive decomp
+
+	p1 = cdr(F);
+	while (iscons(p1)) {
+		push(car(p1));
+		push(X);
+		decomp();
+		p1 = cdr(p1);
+	}
+}
+
+void
+decomp_sum(struct atom *F, struct atom *X)
+{
+	int h, i, j, k, n;
+	struct atom *p1, *p2;
+
+	h = tos;
+
+	// partition terms
+
+	p1 = cdr(F);
+
+	while (iscons(p1)) {
+		p2 = car(p1);
+		if (findf(p2, X)) {
+			if (car(p2) == symbol(MULTIPLY)) {
+				push(p2);
+				push(X);
+				partition_term();	// push const part then push var part
+			} else {
+				push_integer(1);	// const part
+				push(p2);		// var part
+			}
+		}
+		p1 = cdr(p1);
+	}
+
+	// combine const parts of matching var parts
+
+	n = tos - h;
+
+	for (i = 0; i < n - 2; i += 2)
+		for (j = i + 2; j < n; j += 2) {
+			if (!equal(stack[h + i + 1], stack[h + j + 1]))
+				continue;
+			push(stack[h + i]); // add const parts
+			push(stack[h + j]);
+			add();
+			stack[h + i] = pop();
+			for (k = j; k < n - 2; k++)
+				stack[h + k] = stack[h + k + 2];
+			j -= 2; // use same j again
+			n -= 2;
+			tos -= 2; // pop
+		}
+
+	// push const parts, decomp var parts
+
+	list(tos - h);
+	p1 = pop();
+
+	while (iscons(p1)) {
+		push(car(p1)); // const part
+		push(cadr(p1)); // var part
+		push(X);
+		decomp();
+		p1 = cddr(p1);
+	}
+
+	// add together all constant terms
+
+	h = tos;
+	p1 = cdr(F);
+	while (iscons(p1)) {
+		if (!findf(car(p1), X))
+			push(car(p1));
+		p1 = cdr(p1);
+	}
+
+	n = tos - h;
+
+	if (n > 1) {
+		list(n);
+		push_symbol(ADD);
+		swap();
+		cons(); // makes ADD head of list
+	}
+}
+
+void
+decomp_product(struct atom *F, struct atom *X)
+{
+	int h, n;
+	struct atom *p1;
+
+	// decomp factors involving x
+
+	p1 = cdr(F);
+	while (iscons(p1)) {
+		if (findf(car(p1), X)) {
+			push(car(p1));
+			push(X);
+			decomp();
+		}
+		p1 = cdr(p1);
+	}
+
+	// combine constant factors
+
+	h = tos;
+	p1 = cdr(F);
+	while (iscons(p1)) {
+		if (!findf(car(p1), X))
+			push(car(p1));
+		p1 = cdr(p1);
+	}
+
+	n = tos - h;
+
+	if (n > 1) {
+		list(n);
+		push_symbol(MULTIPLY);
+		swap();
+		cons(); // makes MULTIPLY head of list
+	}
+}
+
+void
+partition_term(void)
+{
+	int h, n;
+	struct atom *p1, *F, *X;
+
+	X = pop();
+	F = pop();
+
+	// push const factors
+
+	h = tos;
+	p1 = cdr(F);
+	while (iscons(p1)) {
+		if (!findf(car(p1), X))
+			push(car(p1));
+		p1 = cdr(p1);
+	}
+
+	n = tos - h;
+
+	if (n == 0)
+		push_integer(1);
+	else if (n > 1) {
+		list(n);
+		push_symbol(MULTIPLY);
+		swap();
+		cons(); // makes MULTIPLY head of list
+	}
+
+	// push var factors
+
+	h = tos;
+	p1 = cdr(F);
+	while (iscons(p1)) {
+		if (findf(car(p1), X))
+			push(car(p1));
+		p1 = cdr(p1);
+	}
+
+	n = tos - h;
+
+	if (n == 0)
+		push_integer(1);
+	else if (n > 1) {
+		list(n);
+		push_symbol(MULTIPLY);
+		swap();
+		cons(); // makes MULTIPLY head of list
+	}
 }
 void
 eval_inv(struct atom *p1)
@@ -15614,57 +15667,6 @@ outbuf_putc(int c)
 
 	outbuf[outbuf_index++] = c;
 	outbuf[outbuf_index] = '\0';
-}
-void
-partition_term(void)
-{
-	int h, n;
-	struct atom *p1, *F, *X;
-
-	X = pop();
-	F = pop();
-
-	// push const factors
-
-	h = tos;
-	p1 = cdr(F);
-	while (iscons(p1)) {
-		if (!findf(car(p1), X))
-			push(car(p1));
-		p1 = cdr(p1);
-	}
-
-	n = tos - h;
-
-	if (n == 0)
-		push_integer(1);
-	else if (n > 1) {
-		list(n);
-		push_symbol(MULTIPLY);
-		swap();
-		cons(); // makes MULTIPLY head of list
-	}
-
-	// push var factors
-
-	h = tos;
-	p1 = cdr(F);
-	while (iscons(p1)) {
-		if (findf(car(p1), X))
-			push(car(p1));
-		p1 = cdr(p1);
-	}
-
-	n = tos - h;
-
-	if (n == 0)
-		push_integer(1);
-	else if (n > 1) {
-		list(n);
-		push_symbol(MULTIPLY);
-		swap();
-		cons(); // makes MULTIPLY head of list
-	}
 }
 char *
 read_file(char *filename)
