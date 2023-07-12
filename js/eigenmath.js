a5b435fd07a91f005a3aa0825f6c2305ef29ce4a
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-07-12 16:46:26 -0700

    check for func defn in func

--- js/eigenmath.js
+++ js/eigenmath.js
@@ -1628,81 +1628,6 @@ const ARG7 = "$7";
 const ARG8 = "$8";
 const ARG9 = "$9";
 function
-convert_body(A)
-{
-	if (!iscons(A))
-		return;
-
-	push(car(A));
-	push_symbol(ARG1);
-	subst();
-
-	A = cdr(A);
-	if (!iscons(A))
-		return;
-
-	push(car(A));
-	push_symbol(ARG2);
-	subst();
-
-	A = cdr(A);
-	if (!iscons(A))
-		return;
-
-	push(car(A));
-	push_symbol(ARG3);
-	subst();
-
-	A = cdr(A);
-	if (!iscons(A))
-		return;
-
-	push(car(A));
-	push_symbol(ARG4);
-	subst();
-
-	A = cdr(A);
-	if (!iscons(A))
-		return;
-
-	push(car(A));
-	push_symbol(ARG5);
-	subst();
-
-	A = cdr(A);
-
-	if (!iscons(A))
-		return;
-
-	push(car(A));
-	push_symbol(ARG6);
-	subst();
-
-	A = cdr(A);
-	if (!iscons(A))
-		return;
-
-	push(car(A));
-	push_symbol(ARG7);
-	subst();
-
-	A = cdr(A);
-	if (!iscons(A))
-		return;
-
-	push(car(A));
-	push_symbol(ARG8);
-	subst();
-
-	A = cdr(A);
-	if (!iscons(A))
-		return;
-
-	push(car(A));
-	push_symbol(ARG9);
-	subst();
-}
-function
 copy_tensor(p1)
 {
 	var i, n, p2;
@@ -16678,6 +16603,9 @@ setq_usrfunc(p1)
 	A = cdadr(p1); // function args
 	B = caddr(p1); // function body
 
+	if (find_func_defn(B))
+		stopf("func defn in func");
+
 	if (!isusersymbol(F))
 		stopf("user symbol expected");
 
@@ -16689,6 +16617,103 @@ setq_usrfunc(p1)
 	C = pop();
 
 	set_symbol(F, B, C);
+}
+
+function
+convert_body(A)
+{
+	if (!iscons(A))
+		return;
+
+	push(car(A));
+	push_symbol(ARG1);
+	subst();
+
+	A = cdr(A);
+	if (!iscons(A))
+		return;
+
+	push(car(A));
+	push_symbol(ARG2);
+	subst();
+
+	A = cdr(A);
+	if (!iscons(A))
+		return;
+
+	push(car(A));
+	push_symbol(ARG3);
+	subst();
+
+	A = cdr(A);
+	if (!iscons(A))
+		return;
+
+	push(car(A));
+	push_symbol(ARG4);
+	subst();
+
+	A = cdr(A);
+	if (!iscons(A))
+		return;
+
+	push(car(A));
+	push_symbol(ARG5);
+	subst();
+
+	A = cdr(A);
+
+	if (!iscons(A))
+		return;
+
+	push(car(A));
+	push_symbol(ARG6);
+	subst();
+
+	A = cdr(A);
+	if (!iscons(A))
+		return;
+
+	push(car(A));
+	push_symbol(ARG7);
+	subst();
+
+	A = cdr(A);
+	if (!iscons(A))
+		return;
+
+	push(car(A));
+	push_symbol(ARG8);
+	subst();
+
+	A = cdr(A);
+	if (!iscons(A))
+		return;
+
+	push(car(A));
+	push_symbol(ARG9);
+	subst();
+}
+
+function
+find_func_defn(p1)
+{
+	if (!iscons(p1))
+		return 0;
+
+	if (car(p1) == symbol(SETQ) && caadr(p1) == symbol(INDEX))
+		return 0; // component access
+
+	if (car(p1) == symbol(SETQ) && iscons(cadr(p1)))
+		return 1; // func defn
+
+	while (iscons(p1)) {
+		if (find_func_defn(car(p1)))
+			return 1;
+		p1 = cdr(p1);
+	}
+
+	return 0;
 }
 function
 setup_final(F, T)
