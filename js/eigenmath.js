26eb740722955ff9460530f7cfbe88c2cc87afd2
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-10-27 09:35:31 -0700

    update

--- js/eigenmath.js
+++ js/eigenmath.js
@@ -14059,7 +14059,7 @@ init()
 }
 var init_script = [
 "i = sqrt(-1)",
-"grad(v) = d(v,(x,y,z))",
+"grad(f) = d(f,(x,y,z))",
 "cross(a,b) = (dot(a[2],b[3])-dot(a[3],b[2]),dot(a[3],b[1])-dot(a[1],b[3]),dot(a[1],b[2])-dot(a[2],b[1]))",
 "curl(u) = (d(u[3],y) - d(u[2],z),d(u[1],z) - d(u[3],x),d(u[2],x) - d(u[1],y))",
 "div(u) = d(u[1],x) + d(u[2],y) + d(u[3],z)",
--- src/run.c
+++ src/run.c
@@ -101,7 +101,7 @@ print_trace(int color)
 
 char *init_script =
 "i = sqrt(-1)\n"
-"grad(v) = d(v,(x,y,z))\n"
+"grad(f) = d(f,(x,y,z))\n"
 "cross(a,b) = (dot(a[2],b[3])-dot(a[3],b[2]),dot(a[3],b[1])-dot(a[1],b[3]),dot(a[1],b[2])-dot(a[2],b[1]))\n"
 "curl(u) = (d(u[3],y)-d(u[2],z),d(u[1],z)-d(u[3],x),d(u[2],x)-d(u[1],y))\n"
 "div(u) = d(u[1],x)+d(u[2],y)+d(u[3],z)\n"
