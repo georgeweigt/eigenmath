2454ef9269d486fa5c64cc4735272d5195937674
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2024-03-06 11:37:17 -0700

    update

--- js/eigenmath.js
+++ js/eigenmath.js
@@ -1,7 +1,7 @@
 /*
 BSD 2-Clause License
 
-Copyright (c) 2023, George Weigt
+Copyright (c) 2024, George Weigt
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
